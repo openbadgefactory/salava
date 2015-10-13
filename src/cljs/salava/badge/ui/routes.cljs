23f2ba2bae30078f7deffdab3f075093ca0ae857
Antti Koskinen <antti.koskinen@discendum.com>
2015-10-13 12:27:47 +0300

    Badge translations testing
    Signed-off-by: Antti Koskinen <antti.koskinen@discendum.com>

--- src/cljs/salava/badge/ui/routes.cljs
+++ src/cljs/salava/badge/ui/routes.cljs
@@ -5,12 +5,12 @@
   [:p "Got badge id " badge-id])
 
 (defn ^:export routes [context]
-  {"/badge" [["/"        (constantly [:p (t :badge/mybadges)])]
+  {"/badge" [["/"        (constantly [:p (t :badge/Mybadges)])]
              [["/show/" :badge-id] show-badge]
-             ["/import/" (constantly [:p (t :badge/importbadges)])]
-             ["/upload/" (constantly [:p (t :badge/uploadbadges)])]
-             ["/export/" (constantly [:p (t :badge/exportbadges)])]
-             ["/stats/"  (constantly [:p (t :badge/badgestats)])]]})
+             ["/import/" (constantly [:p (t :badge/Importbadges)])]
+             ["/upload/" (constantly [:p (t :badge/Uploadbadges)])]
+             ["/export/" (constantly [:p (t :badge/Exportbadges)])]
+             ["/stats/"  (constantly [:p (t :badge/Badgestats)])]]})
 
 
 (defn ^:export navi [context]
