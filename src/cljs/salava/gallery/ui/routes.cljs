83e4ac0e0a88184d8116dfb851e57efc6df54153
Antti Koskinen <antti.koskinen@discendum.com>
2015-11-11 11:26:38 +0200

    Gallery module, new translation string builder

--- src/cljs/salava/gallery/ui/routes.cljs
+++ src/cljs/salava/gallery/ui/routes.cljs
@@ -14,9 +14,9 @@
                ["/getbadge" (placeholder [:p "Apply for a badge"])]]})
 
 (defn ^:export navi [context]
-  {"/gallery"          {:weight 40 :title (t :gallery/Gallery)          :breadcrumb (str (t :gallery/Gallery) " / " (t :gallery/Sharedbadges))}
-   "/gallery/badges"   {:weight 41 :title (t :gallery/Sharedbadges)     :breadcrumb (str (t :gallery/Gallery) " / " (t :gallery/Sharedbadges))}
-   "/gallery/pages"    {:weight 42 :title (t :gallery/Sharedpages)      :breadcrumb (str (t :gallery/Gallery) " / " (t :gallery/Sharedpages))}
-   "/gallery/profiles" {:weight 43 :title (t :gallery/Sharedprofiles)   :breadcrumb (str (t :gallery/Gallery) " / " (t :gallery/Sharedprofiles))}
-   "/gallery/getbadge" {:weight 44 :title (t :gallery/Applybadge)       :breadcrumb (str (t :gallery/Gallery) " / " (t :gallery/Applybadge))}})
+  {"/gallery"          {:weight 40 :title (t :gallery/Gallery)          :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedbadges)}
+   "/gallery/badges"   {:weight 41 :title (t :gallery/Sharedbadges)     :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedbadges)}
+   "/gallery/pages"    {:weight 42 :title (t :gallery/Sharedpages)      :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedpages)}
+   "/gallery/profiles" {:weight 43 :title (t :gallery/Sharedprofiles)   :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedprofiles)}
+   "/gallery/getbadge" {:weight 44 :title (t :gallery/Applybadge)       :breadcrumb (t :gallery/Gallery " / " :gallery/Applybadge)}})
 
