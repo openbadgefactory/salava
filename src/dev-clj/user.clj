6f67fad639ff70602a68592dcc3691718f18d1cb
Perttu Piirainen <perttu.piirainen@discendum.com>
2015-11-27 15:59:35 +0200

    Disable reloading of tests when resetting. Handle exceptions while running tests.

--- src/dev-clj/user.clj
+++ src/dev-clj/user.clj
@@ -1,5 +1,8 @@
 (ns user
-  (:require [reloaded.repl :refer [system init start stop go reset]]))
+  (:require [reloaded.repl :refer [system init start stop go reset]]
+            [clojure.tools.namespace.repl :refer [set-refresh-dirs]]))
+
+(set-refresh-dirs "./src")
 
 (defn set-opts [& {:keys [system opts]
                    :or {system 'salava.core.system/base-system
--- test/clj/salava/test_utils.clj
+++ test/clj/salava/test_utils.clj
@@ -1,8 +1,9 @@
 (ns salava.test-utils
   (:require [clj-http.client :as client]
+            [slingshot.slingshot :refer :all]
             [salava.core.components.config :as c]))
 
-(def api-host
+(def api-root
   (let [config (c/load-config :core)
         host (get-in config [:http :host])
         port (get-in config [:http :port])]
@@ -11,20 +12,24 @@
 (defn test-api-request
   ([method url] (test-api-request method url {}))
   ([method url post-params]
-   (client/request
-     {:method       method
-      :url          (str api-host url)
-      :content-type :json
-      :as :json
-      :form-params  post-params
-      :throw-exceptions false})))
+   (try+
+     (client/request
+       {:method       method
+        :url          (str api-root url)
+        :content-type :json
+        :as :json
+        :form-params  post-params
+        :throw-exceptions false})
+     (catch Exception _))))
 
 (defn test-upload [url upload-data]
-  (client/request
-    {:method           :post
-     :url              (str api-host url)
-     :multipart        upload-data
-     :as               :json
-     :throw-exceptions false}))
+  (try+
+    (client/request
+      {:method           :post
+       :url              (str api-root url)
+       :multipart        upload-data
+       :as               :json
+       :throw-exceptions false})
+    (catch Exception _)))
 
 
