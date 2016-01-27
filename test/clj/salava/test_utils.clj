3c9e5682413eed07d3a1ad5322b664f40b27b0a3
Perttu Piirainen <perttu.piirainen@discendum.com>
2016-01-27 11:39:23 +0200

    Authentication support for unit-tests

--- test/clj/salava/test_utils.clj
+++ test/clj/salava/test_utils.clj
@@ -1,5 +1,6 @@
 (ns salava.test-utils
   (:require [clj-http.client :as client]
+            [clj-http.cookies :as cookies]
             [slingshot.slingshot :refer :all]
             [salava.core.components.config :as c]))
 
@@ -8,6 +9,24 @@
         host (get-in config [:http :host])
         port (get-in config [:http :port])]
     (str "http://" host ":"port "/obpv1" )))
+
+(def ^:dynamic *cookie-store*
+  (cookies/cookie-store))
+
+(defn login! [email password]
+  (client/request {:url (str api-root "/user/login")
+                   :method :post
+                   :form-params {:email email :password password}
+                   :as :json
+                   :content-type :json
+                   :cookie-store *cookie-store*}))
+
+(defn logout! []
+  (client/request {:url (str api-root "/user/logout")
+                   :method :post
+                   :as :json
+                   :content-type :json
+                   :cookie-store *cookie-store*}))
 
 (defn test-api-request
   ([method url] (test-api-request method url {}))
@@ -19,7 +38,8 @@
         :content-type :json
         :as :json
         :form-params  post-params
-        :throw-exceptions false})
+        :throw-exceptions false
+        :cookie-store *cookie-store*})
      (catch Exception _))))
 
 (defn test-upload [url upload-data]
@@ -29,7 +49,25 @@
        :url              (str api-root url)
        :multipart        upload-data
        :as               :json
-       :throw-exceptions false})
+       :throw-exceptions false
+       :cookie-store *cookie-store*})
     (catch Exception _)))
+
+(def test-users
+  [{:id 1
+    :email "test-user@example.com"
+    :password "testtest"}
+   {:id 2
+    :email "another-user@example.com"
+    :password "testtest"}
+   {:id 3
+    :email "third-user@example.com"
+    :password "testtest"}])
+
+(defn test-user-credentials [user-id]
+  (let [user (->> test-users
+                  (filter #(= (:id %) user-id))
+                  first)]
+    [(:email user) (:password user)]))
 
 
