2638a37e53a14e294bb493983b6e8b538670d964
Perttu Piirainen <perttu.piirainen@discendum.com>
2016-04-12 13:09:48 +0300

    Remove 'Apply for a badge' -link

--- src/cljs/salava/gallery/ui/routes.cljs
+++ src/cljs/salava/gallery/ui/routes.cljs
@@ -17,15 +17,13 @@
                ["/pages"    p/handler]
                [["/pages/" :user-id] p/handler]
                ["/profiles" u/handler]
-               [["/badgeview/" :badge-content-id] bv/handler]
-               ["/getbadge" (placeholder [:p "Apply for a badge"])]]})
+               [["/badgeview/" :badge-content-id] bv/handler]]})
 
 (defn ^:export navi [context]
   {"/gallery"          {:weight 40 :title (t :gallery/Gallery)        :top-navi true  :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedbadges)}
    "/gallery/badges"   {:weight 41 :title (t :gallery/Sharedbadges)   :site-navi true :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedbadges)}
    "/gallery/pages"    {:weight 42 :title (t :gallery/Sharedpages)    :site-navi true :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedpages)}
    "/gallery/profiles" {:weight 43 :title (t :gallery/Sharedprofiles) :site-navi true :breadcrumb (t :gallery/Gallery " / " :gallery/Sharedprofiles)}
-   "/gallery/getbadge" {:weight 44 :title (t :gallery/Applybadge)     :site-navi true :breadcrumb (t :gallery/Gallery " / " :gallery/Applybadge)}
    "/gallery/badges/\\d+" {:breadcrumb (t :gallery/Gallery " / " :gallery/Sharedbadges)}
    "/gallery/pages/\\d+" {:breadcrumb (t :gallery/Gallery " / " :gallery/Sharedpages)}
    "/gallery/badgeview/\\w+" {:breadcrumb (t :gallery/Gallery " / " :gallery/Viewbadge)}})
