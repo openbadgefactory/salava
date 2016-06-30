70d666d6283ac38d16520a859a04c4941f3e3582
Leo Vainio <leo.vainio@discendum.com>
2016-06-30 09:41:40 +0300

    fix sql " to '

--- resources/sql/displayer/queries.sql
+++ resources/sql/displayer/queries.sql
@@ -4,13 +4,13 @@ SELECT user_id FROM user_email WHERE email = :email AND verified = 1
 
 -- name: select-public-badge-count
 -- Get count of the user's public badges
-SELECT COUNT(id) AS count FROM badge WHERE visibility = "public" AND deleted = 0 AND user_id = :user_id
+SELECT COUNT(id) AS count FROM badge WHERE visibility = 'public' AND deleted = 0 AND user_id = :user_id
 
 -- name: select-public-badge-groups
 -- Get user badge tags which are associated to public badges
 SELECT bt.tag AS name, MAX(bt.id) AS id, COUNT(DISTINCT b.id) AS badges FROM badge AS b
        JOIN badge_tag AS bt ON (bt.badge_id = b.id)
-       WHERE b.user_id = :user_id AND b.deleted = 0 AND b.visibility = "public"
+       WHERE b.user_id = :user_id AND b.deleted = 0 AND b.visibility = 'public'
        GROUP BY bt.tag
 
 --name: select-badge-tag
