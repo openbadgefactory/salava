82f4ec84fce253ece9c41803eecc49aa1760df72
Leo Vainio <leo.vainio@discendum.com>
2016-07-21 12:28:51 +0300

    Add Landing page navbar items Home/About/News/faq

--- src/cljs/salava/extra/passport/ui/routes.cljs
+++ src/cljs/salava/extra/passport/ui/routes.cljs
@@ -1,7 +1,12 @@
 (ns salava.extra.passport.ui.routes
-  (:require [salava.extra.passport.ui.footer :refer [footer]]))
+  (:require [salava.extra.passport.ui.footer :refer [footer]]
+            [salava.core.i18n :as i18n :refer [t]]))
 
 (defn ^:export routes [context] {})
 
 (defn ^:export navi [context]
-  {"passport/footer" {:footer footer}})
+  {"passport/footer" {:footer footer}
+   "https://openbadgepassport.com/" {:weight 1 :title (t :core/Home) :top-navi-landing true}
+   "https://openbadgepassport.com/about/" {:weight 2 :title (t :core/About) :top-navi-landing true}
+   "https://openbadgepassport.com/news/" {:weight 3 :title (t :core/News) :top-navi-landing true}
+   "https://openbadgepassport.com/faq/" {:weight 4 :title (t :core/Faq) :top-navi-landing true}})
