19e1fe90951bdb22c985be8dd2a531cf64729a9b
Leo Vainio <leo.vainio@discendum.com>
2016-10-18 16:42:11 +0300

    Fix some routing problems

--- src/cljs/salava/extra/passport/ui/routes.cljs
+++ src/cljs/salava/extra/passport/ui/routes.cljs
@@ -8,7 +8,6 @@
 
 (defn ^:export navi [context]
   {"passport/footer" {:footer footer}
-   "https://openbadgepassport.com/" {:weight 1 :title (t :core/Home) :top-navi-landing true}
    "https://openbadgepassport.com/about/" {:weight 2 :title (t :core/About) :top-navi-landing true}
    "https://openbadgepassport.com/news/" {:weight 3 :title (t :core/News) :top-navi-landing true}
    "https://openbadgepassport.com/faq/" {:weight 4 :title (t :core/Faq) :top-navi-landing true}})
