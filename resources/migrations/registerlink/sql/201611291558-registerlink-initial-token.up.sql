78b9ce579db5826f0ef383d50b26da3d4b88eedf
Leo Vainio <leo.vainio@discendum.com>
2016-11-30 09:52:16 +0200

    Fix console error and make initial registerlink token

---
+++ resources/migrations/registerlink/sql/201611291558-registerlink-initial-token.up.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO config (name, value)
+       	      	   	  VALUES ('register-token', 'ad-ad-moo')
+--;;
+
+INSERT IGNORE INTO config (name, value)
+       	      	   	  VALUES ('register-active', '1')
+--;;
