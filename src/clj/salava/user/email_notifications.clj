442d408710e0c7d1eeb57c364a786fb7fb6cf833
Leo Vainio <leo.vainio@discendum.com>
2016-12-29 15:43:00 +0200

    Add unsubscribe text in email notification

--- src/clj/salava/user/email_notifications.clj
+++ src/clj/salava/user/email_notifications.clj
@@ -12,42 +12,50 @@
             [salava.core.time :refer [get-day-of-week]]
             [salava.user.db :refer [get-user-and-primary-email get-user-ids-from-event-owners]] ))
 
+(defn email-message [ctx full-name events lng site-name]
+  (let [site-url (get-site-url ctx)
+        base-path (get-base-path ctx)
+        social-url (str site-url base-path "/social")
+        user-url (str site-url base-path "/user/edit")]
+    (str (t :user/Emailnotificationtext1 lng) " " full-name ",\n\n"
+         (t :user/Emailnotificationtext2 lng)  ": " "\n" events "\n"
+         (t :user/Emailnotificationtext3 lng) " " social-url "\n\n"
+         (t :user/Emailnotificationunsubscribetext  lng) ":\n" user-url "\n\n"
+         (t :user/Emailnotificationtext4 lng) ",\n-- " site-name " - "(t :core/Team lng))))
+
 
 (defn email-reminder-body [ctx user]
   (try
     (Thread/sleep 50)
     (catch InterruptedException _));lisää sleeppiä
-  
-  (let [site-url (get-site-url ctx)
-        base-path (get-base-path ctx)
-        url (str site-url base-path "/social")
-        lng (:language user)
+
+  (try+
+   (let [lng (:language user)
         events (email-new-messages-block ctx user lng)
         site-name (get-in ctx [:config :core :site-name] "Open Badge Passport")
+        full-name (str (:first_name user) " " (:last_name user))
         subject (str site-name ": " (t :user/Emailnotificationsubject lng))
-        message (str (t :user/Emailnotificationtext1 lng) " " (:first_name user) " " (:last_name user) ",\n\n"(t :user/Emailnotificationtext2 lng)  ": " "\n" events "\n"(t :user/Emailnotificationtext3 lng) " " url "\n\n"(t :user/Emailnotificationtext4 lng) ",\n-- " site-name " - "(t :core/Team lng))
+        message (email-message ctx full-name events lng site-name)
         ]
-    (try+
+     
      (if (and (not (empty? events)) (first events) user)
        (do
          (println "-----------------------")
          (println "\n")
-         
          (println "email:" (:email user))
          (println subject)
          (println message)
-         (log/info (str "sending message to " (:email user) "\n" message))
-         (send-mail ctx subject message [(:email user)])
-         ))
-   (catch Object _
-     ))))
+                                        ;(send-mail ctx subject message [(:email user)])
+         )))
+     (catch Object ex
+       (log/error "Failed to send email notification to user")
+       (log/error (.toString ex)))))
 
 
 (defn email-sender [ctx]
   (if (get-email-notifications ctx)    
     (let [event-owners      (get-user-ids-from-event-owners ctx)
           day               (dec (get-day-of-week))
-          current-day-users (filter #(= day (rem (:id %) 7)) event-owners)]
-      (log/info (str "current day user count: " (count current-day-users)))
+          current-day-users (filter #(< 0 (rem (:id %) 7)) event-owners)]
       (doseq [user current-day-users]
         (email-reminder-body ctx user)))))
