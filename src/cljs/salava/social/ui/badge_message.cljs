1e219006edbb8c3284e74f0a764b5b28758af443
Leo Vainio <leo.vainio@discendum.com>
2016-12-30 15:42:14 +0200

    Fix code stylish

--- src/cljs/salava/social/ui/badge_message.cljs
+++ src/cljs/salava/social/ui/badge_message.cljs
@@ -11,19 +11,17 @@
             [salava.core.i18n :refer [t]]
             [salava.core.helper :refer [dump]]
             [salava.social.ui.follow :as f]
-            [salava.core.time :refer [date-from-unix-time]]
-            
-            ))
+            [salava.core.time :refer [date-from-unix-time]]))
 
 (defn init-data [state]
   (ajax/GET
    (path-for (str "/obpv1/social/messages/" (:badge_content_id @state) "/" (:page_count @state)))
    {:handler (fn [data]
-               (swap! state assoc :messages (into (:messages @state) (:messages data))
-                                 :message ""
-                                 :page_count (inc (:page_count @state))
-                                 :messages_left (:messages_left data)))})
-  )
+               (swap! state assoc
+                      :messages (into (:messages @state) (:messages data))
+                      :message ""
+                      :page_count (inc (:page_count @state))
+                      :messages_left (:messages_left data)))}))
 
 
 (defn save-message [state]
@@ -39,28 +37,25 @@
                    (if (= "success" (:connected? data))
                      (do
                        (f/init-data badge_content_id)
-                       (swap! state assoc :start-following true)
-                       )
+                       (swap! state assoc :start-following true))
                      (swap! state assoc :start-following false))
-                   (swap! state assoc :messages []
-                                      :page_count 0)
+                   (swap! state assoc
+                          :messages []
+                          :page_count 0)
                    (init-data state)))
       :error-handler (fn [{:keys [status status-text]}]
-                       )})))
+                       (dump (str status " " status-text)))})))
 
 (defn delete-message [id state]
-(ajax/POST
-     (path-for (str "/obpv1/social/delete_message/" id))
-     {:response-format :json
-      :keywords? true 
-      :handler (fn [data]
-                 (do
-                   (let [filtered-messages (filter #(not (= id (:id %))) (:messages @state))]
-                     (swap! state assoc :messages filtered-messages))
-                   ))
-      :error-handler (fn [{:keys [status status-text]}]
-                       )})
-)
+  (ajax/POST
+   (path-for (str "/obpv1/social/delete_message/" id))
+   {:response-format :json
+    :keywords? true 
+    :handler (fn [data]
+               (let [filtered-messages (filter #(not (= id (:id %))) (:messages @state))]
+                 (swap! state assoc :messages filtered-messages)))
+    :error-handler (fn [{:keys [status status-text]}]
+                    (dump (str status " " status-text)))}))
 
 (defn delete-message-button [id state]
   (let [delete-clicked (atom nil)]
@@ -71,10 +66,8 @@
                  :aria-label "OK"
                  :on-click   #(do
                                 (reset! delete-clicked (if (= true @delete-clicked) nil true))
-                                (.preventDefault %))
-                 }
-        [:span {:aria-hidden "true"
-                
+                                (.preventDefault %))}
+        [:span {:aria-hidden "true"  
                 :dangerouslySetInnerHTML {:__html "&times;"}}]]
        (if @delete-clicked
          [:div
@@ -82,13 +75,11 @@
            (t :badge/Confirmdelete)]
           [:button {:type  "button"
                     :class "btn btn-primary"
-                    :on-click #(reset! delete-clicked nil)
-                    }
+                    :on-click #(reset! delete-clicked nil)}
            (t :badge/Cancel)]
           [:button {:type  "button"
                     :class "btn btn-warning"
-                    :on-click     #(delete-message id state)
-                    }
+                    :on-click     #(delete-message id state)}
            (t :badge/Delete)]])])))
 
 (defn blank-reduce [a-seq]
@@ -108,21 +99,19 @@
 
 (defn message-list-item [{:keys [message first_name last_name ctime id profile_picture user_id]} state]
   [:div {:class "media message-item" :key id}
-  
    [:div.msgcontent
-   [:span {:class "pull-left"}
-    [:img {:class "message-profile-img" :src (profile-picture profile_picture)}]]
-   [:div {:class "media-body"}
-    [:h4 {:class "media-heading"}
+    [:span {:class "pull-left"}
+     [:img {:class "message-profile-img" :src (profile-picture profile_picture)}]]
+    [:div {:class "media-body"}
+     [:h4 {:class "media-heading"}
       [:a {:href (path-for (str "/user/profile/" user_id)) :target "_blank"} (str first_name " "last_name)]
-      [:span.date (date-from-unix-time (* 1000 ctime) "minutes")]
-     ]
-    (into [:div] (for [ item (clojure.string/split-lines message)]
-                   (into [:p.msg] (if (or (re-find #"www." item) (re-find #"https?://" item) (re-find #"http?://" item)) 
-                                      (search-and-replace-www item)
-                                      item))))]]
-    (if (or (=  user_id (:user_id @state)) (= "admin" (:user_role @state)))
-        [delete-message-button id state])])
+      [:span.date (date-from-unix-time (* 1000 ctime) "minutes")]]
+     (into [:div] (for [ item (clojure.string/split-lines message)]
+                    (into [:p.msg] (if (or (re-find #"www." item) (re-find #"https?://" item) (re-find #"http?://" item)) 
+                                     (search-and-replace-www item)
+                                     item))))]]
+   (if (or (=  user_id (:user_id @state)) (= "admin" (:user_role @state)))
+     [delete-message-button id state])])
 
 (defn message-list-load-more [state]
   (if (pos? (:messages_left @state))
@@ -133,7 +122,6 @@
                   :on-click #(do
                                (init-data state)
                                (.preventDefault %))}
-
               (str (t :social/Loadmore) " (" (:messages_left @state) " " (t :social/Messagesleft) ")")]]]]))
 
 
@@ -148,8 +136,7 @@
                                     (doall
                                      (for [item messages]
                                        (message-list-item item state)))
-                                    (message-list-load-more state)
-                                    ])
+                                    (message-list-load-more state)])
                  ;:component-did-mount #(scroll-bottom)
                  ;:component-did-update #(scroll-bottom)
                  }))
@@ -178,29 +165,31 @@
                     (init-data state)
                     (.preventDefault %))} "Refresh"])
 
+(defn start-following-alert [state]
+  (let [{:keys [badge_content_id]} @state]
+    [:div {:class (str "alert ""alert-success")}
+     [:div.deletemessage
+      [:button {:type       "button"
+                :class      "close"
+                :aria-label "OK"
+                :on-click   #(do
+                               (swap! state assoc :start-following false)
+                               (.preventDefault %))}
+       [:span {:aria-hidden "true"
+               :dangerouslySetInnerHTML {:__html "&times;"}}]]]
+     (str (t :social/Youstartedfollowbadge) "! ")
+     [:a {:href "#"
+          :on-click #(do
+                       (f/unfollow-ajax-post badge_content_id)
+                       (swap! state assoc :start-following false))}
+      (t :social/Cancelfollowingbadge)]]))
+
 (defn content [state]
-  (let [{:keys [messages badge_content_id start-following]} @state]
+  (let [{:keys [messages start-following]} @state]
     [:div
      (message-textarea state)
-                                        ;(refresh-button state)
      (if start-following
-       [:div {:class (str "alert ""alert-success")}
-        [:div.deletemessage
-         [:button {:type       "button"
-                   :class      "close"
-                   :aria-label "OK"
-                   :on-click   #(do
-                                  (swap! state assoc :start-following false)
-                                  (.preventDefault %))
-                   }
-          [:span {:aria-hidden "true"
-                  :dangerouslySetInnerHTML {:__html "&times;"}}]]
-       ]
-        (str (t :social/Youstartedfollowbadge) "! ")
-        [:a {:href "#" :on-click #(do
-                                    (f/unfollow-ajax-post badge_content_id)
-                                    (swap! state assoc :start-following false))} (t :social/Cancelfollowingbadge)]]
-       )
+       (start-following-alert state))
      [message-list messages state]]))
 
 
@@ -216,5 +205,4 @@
                      :start-following false})]
     (init-data state)
     (fn []
-      (content state)
-      )))
+      (content state))))
