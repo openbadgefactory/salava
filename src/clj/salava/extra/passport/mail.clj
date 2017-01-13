65055dac083cd424adf690cd3461b6381bbf45aa
Leo Vainio <leo.vainio@discendum.com>
2017-01-13 13:38:06 +0200

    Create extra/passport have own banner

---
+++ src/clj/salava/extra/passport/mail.clj
@@ -0,0 +1,27 @@
+(ns salava.extra.passport.mail
+  (:require [salava.core.i18n :refer [t]]
+            [salava.core.util :refer [get-site-url]]))
+
+
+
+
+
+
+
+(defn html-mail-banner [ctx]
+  (let [site-url (get-site-url ctx)]
+    [:div
+     {:style
+      "margin-top: 10px;margin-bottom: 50px;padding-top: 0;padding-bottom: 0;"}
+     [:a {:href site-url :target "_blank" :style "text-decoration: none;"}
+      [:img.banner
+       {:alt    "Open Badge Passport",
+        :style  "max-width: 640px;",
+        :src    (str site-url "/img/extra/passport/logo.png"),
+        :height "auto",
+        :width  "auto"}]]]))
+
+(defmulti get-fragment #(last %&))
+
+(defmethod get-fragment "mail-banner" [ctx user lng type]
+  (html-mail-banner ctx))
