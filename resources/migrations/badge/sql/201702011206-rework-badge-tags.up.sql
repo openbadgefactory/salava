fde413bb4ffd93d3a0fee9bd6f8b894fe15a3488
Leo Vainio <leo.vainio@discendum.com>
2017-02-22 11:46:57 +0200

    Add tag migrates

---
+++ resources/migrations/badge/sql/201702011206-rework-badge-tags.up.sql
@@ -0,0 +1,13 @@
+rename table badge_content_tag to badge_content_tag_old;
+--;;
+
+CREATE TABLE `badge_content_tag` (
+  `badge_content_id` varchar(255) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`badge_content_id`,`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--;;
+
+INSERT IGNORE INTO badge_content_tag (badge_content_id, tag)
+SELECT badge_content_id, tag FROM badge_content_tag_old;
