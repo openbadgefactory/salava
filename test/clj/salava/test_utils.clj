0ac2f52101ba0c172425ea82c0461d434327a7dc
Antti Koskinen <antti.koskinen@discendum.com>
2017-03-02 12:06:09 +0200

    Use deftest-ctx macro

--- test/clj/salava/test_utils.clj
+++ test/clj/salava/test_utils.clj
@@ -1,5 +1,8 @@
 (ns salava.test-utils
-  (:require [clj-http.client :as client]
+  (:require [clojure.test]
+            [clj-http.client :as client]
+            [clojure.tools.logging :refer  [*logger-factory*]]
+            [clojure.tools.logging.impl :refer  [disabled-logger-factory]]
             [clj-http.cookies :as cookies]
             [ring.mock.request :as mock]
             [com.stuartsierra.component :as component]
@@ -11,93 +14,44 @@
 
 
 (defn get-system []
-  (-> (salava.core.system/base-system "test_config")
-      (dissoc :http-server)
-      (component/start)))
+  (binding [*logger-factory* disabled-logger-factory]
+    (-> (salava.core.system/base-system "test_config")
+        (dissoc :http-server :cron)
+        (component/start))))
 
 (defn stop-system [system]
-  (migrator/reset-seeds (migrator/test-config))
-  (component/stop system))
+   (binding [*logger-factory* disabled-logger-factory]
+      (component/stop system)))
 
 
 (defn parse-body [body]
   (if body
-    (cheshire/parse-string (slurp body) true)))
+    (try
+      (cheshire/parse-string (slurp body) true)
+      (catch Exception _
+        body))))
 
 (defn test-api-request
-  ([system method url] (test-api-request system method url {} ""))
-  ([system method url post-params login-token]
-   (try+
-    (let [response ((get-in system [:handler :handler]) (-> (mock/request method url)
-                                                            (mock/header "cookie" login-token)
-                                                            (mock/content-type "application/json")
-                                                            (mock/body  (cheshire/generate-string post-params))))]
-      (update response :body parse-body))
-    (catch Exception _))))
-
-(defn parse-cookie [headers]
-  (-> headers
-      (get  "Set-Cookie")
-      first
-      str
-      (split  #";")
-      first))
-
-(:headers response)
-(defn login [system email password]
-  (let [response (test-api-request system :post "/app/obpv1/user/login" {:email email :password password} "")
-        cookie (if (:headers response)
-                 (parse-cookie (:headers response))
-                 nil)]
-    (assoc response :cookie cookie)))
-
-(defn test-upload [url upload-data]
-  (try+
-    (client/request
-      {:method           :post
-       :url              (str api-root url)
-       :multipart        upload-data
-       :as               :json
-       :throw-exceptions false
-       :cookie-store *cookie-store*})
-    (catch Exception _)))
-
-(def test-users
-  [{:id 1
-    :email "test.user@example.com"
-    :password "testtest"}
-   {:id 2
-    :email "another.user@example.com"
-    :password "testtest"}
-   {:id 3
-    :email "third.user@example.com"
-    :password "testtest"}
-   {:id 4
-    :email "fourth.user@example.com"
-    :password "testtest"}
-   {:id 5
-    :email "fifth.user@example.com"
-    :password "testtest"}
-   {:id 6
-    :email "sixth.user@example.com"
-    :password "testtest"}
-   {:id 7
-    :email "seventh.user@example.com"
-    :password "testtest"}
-   {:id 8
-    :email "eight.user@example.com"
-    :password "testtest"}
-   {:id 9
-    :email "test.discendum@gmail.com"
-    :password "testtest"}])
+  ([ctx method url] (test-api-request ctx method url {}))
+  ([ctx method url opt]
+   (let [handler (get-in (meta ctx) [:system :handler :handler])]
+     (-> (mock/request method url)
+         (mock/content-type "application/json")
+         (mock/body (cheshire/generate-string (:params opt)))
+         (assoc :identity (:user opt))
+         handler
+         (update :body parse-body)))))
 
 
-(defn login-with-user [system user-id]
-  (let [user (->> test-users
-                  (filter #(= (:id %) user-id))
-                  first)]
-    (login system (:email user) (:password user))))
-
-
-
-
+(defmacro deftest-ctx
+  [name bind & body]
+  `(clojure.test/deftest ~name
+     (let [system# (get-system)
+           ~(first bind) (with-meta {:config     (get-in system# [:config :config])
+                                     :db         (get-in system# [:db :datasource])
+                                     :input-chan (get-in system# [:pubsub :channel :input-chan])}
+                                    {:system system#})]
+       (try
+         ~@body
+         (finally
+           (stop-system system#))))))
