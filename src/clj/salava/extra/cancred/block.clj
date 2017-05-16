6990c602bddaa086325e89b95e78d9a23a5ecbef
Leo Vainio <leo.vainio@discendum.com>
2017-05-16 14:42:31 +0300

    Create favicon support for themes

---
+++ src/clj/salava/extra/cancred/block.clj
@@ -0,0 +1,8 @@
+(ns salava.extra.cancred.block
+  #_(:require [salava.core.i18n :refer [t]]
+            [salava.core.util :refer [get-site-url]]))
+
+
+(defn favicon []
+  {:icon "/img/extra/cancred/favicon.icon"
+   :png "/img/extra/cancred/favicon.png" } )
---
+++ src/clj/salava/extra/passport/block.clj
@@ -0,0 +1,8 @@
+(ns salava.extra.passport.block
+  #_(:require [salava.core.i18n :refer [t]]
+            [salava.core.util :refer [get-site-url]]))
+
+
+(defn favicon []
+  {:icon "/img/extra/passport/favicon.icon"
+   :png "/img/extra/passport/favicon.png" } )
