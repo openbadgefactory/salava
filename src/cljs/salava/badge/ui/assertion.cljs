32d02b2b30c1d44b559dd7ad656672f204c506e9
Leo Vainio <leo.vainio@discendum.com>
2017-06-21 10:30:47 +0300

    Fix user-badge-modal metadata view

--- src/cljs/salava/badge/ui/assertion.cljs
+++ src/cljs/salava/badge/ui/assertion.cljs
@@ -1,6 +1,36 @@
 (ns salava.badge.ui.assertion
   (:require [salava.core.i18n :refer [t]]))
 
+(defn assertion-content [assertion]
+  (fn []
+    [:table {:id "assertion-info"}
+     [:thead
+      [:tr
+       [:th (t :badge/Property)]
+       [:th (t :badge/Value)]]]
+     (into
+      [:tbody]
+      (for [prop-1 (keys assertion)
+            :let   [val-1 (get assertion prop-1)]]
+        (if (map? val-1)
+          (for [prop-2 (keys val-1)
+                :let   [val-2 (get-in assertion [prop-1 prop-2])]]
+            (if (map? val-2)
+              (for [prop-3 (keys val-2)
+                    :let   [val-3 (get-in assertion [prop-1 prop-2 prop-3])]]
+                (if-not (map? val-3)
+                  [:tr {:key (str name prop-3) :class (str "tr-" prop-1 "-" prop-2 "-" prop-3)}
+                   [:td (name prop-1) " " (name prop-2) " " (name prop-3)]
+                   [:td (or val-3 "-")]]))
+              [:tr {:key (str name prop-2) :class (str "tr-" prop-1 "-" prop-2)}
+               [:td (str (name prop-1) " " (name prop-2))]
+               [:td  (or (str val-2) "-")]
+               ]))
+          [:tr {:key (str name prop-1) :class (str "tr-" prop-1)}
+           [:td (name prop-1)]
+           [:td (or (str val-1) "-")]
+           ])))]))
+
 (defn assertion-modal [assertion]
   [:div {:id "badge-settings"}
    [:div.modal-body
@@ -13,33 +43,7 @@
                 }
        [:span {:aria-hidden             "true"
                :dangerouslySetInnerHTML {:__html "&times;"}}]]]]
-    [:table {:id "assertion-info"}
-     [:thead
-      [:tr
-       [:th (t :badge/Property)]
-       [:th (t :badge/Value)]]]
-     (into
-       [:tbody]
-       (for [prop-1 (keys assertion)
-             :let [val-1 (get assertion prop-1)]]
-         (if (map? val-1)
-           (for [prop-2 (keys val-1)
-                 :let [val-2 (get-in assertion [prop-1 prop-2])]]
-             (if (map? val-2)
-               (for [prop-3 (keys val-2)
-                     :let [val-3 (get-in assertion [prop-1 prop-2 prop-3])]]
-                 (if-not (map? val-3)
-                   [:tr {:class (str "tr-" prop-1 "-" prop-2 "-" prop-3)}
-                    [:td (name prop-1) " " (name prop-2) " " (name prop-3)]
-                    [:td (or val-3 "-")]]))
-               [:tr {:class (str "tr-" prop-1 "-" prop-2)}
-                [:td (str (name prop-1) " " (name prop-2))]
-                [:td  (or (str val-2) "-")]
-                ]))
-           [:tr {:class (str "tr-" prop-1)}
-            [:td (name prop-1)]
-            [:td (or (str val-1) "-")]
-            ])))]]
+    [assertion-content assertion]]
    [:div.modal-footer
     [:button {:type         "button"
               :class        "btn btn-primary"
