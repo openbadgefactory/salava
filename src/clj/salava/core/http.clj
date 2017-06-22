cbe0027aed67ed14049594bbf53df076af18aa39
Antti Koskinen <antti.koskinen@discendum.com>
2017-06-22 10:29:10 +0300

    Removed debug logging

--- src/clj/salava/core/http.clj
+++ src/clj/salava/core/http.clj
@@ -54,7 +54,7 @@
        (curl url opt)))))
 
 (defn json-get [url]
-  (log/info "json-get: GET" url)
+  ;(log/info "json-get: GET" url)
   (http-get url {:as :json :accept :json :throw-entire-message? true}))
 
 
