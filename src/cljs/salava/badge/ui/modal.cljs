9f904b6de79fd038f8ecf0ed60c22318665f13e4
Leo Vainio <leo.vainio@discendum.com>
2017-06-22 12:33:45 +0300

    Fix mobile views

--- src/cljs/salava/badge/ui/modal.cljs
+++ src/cljs/salava/badge/ui/modal.cljs
@@ -40,9 +40,10 @@
         selected-language (cursor state [:content-language])
         {:keys [name description tags criteria_content image_file image_file issuer_content_name issuer_content_url issuer_contact issuer_image issuer_description criteria_url  creator_name creator_url creator_email creator_image creator_description message_count]} (content-setter @selected-language content)]
     [:div
+     
      [:div {:id "badge-contents"}
-      [:div {:class "pull-right text-right"}
-       [follow-badge badge_id]]]
+      [:div.pull-right
+      [follow-badge badge_id]]]
      [:div {:id "badge-info"}
       [:div.panel
        [:div.panel-body
--- src/cljs/salava/gallery/ui/modal.cljs
+++ src/cljs/salava/gallery/ui/modal.cljs
@@ -31,70 +31,72 @@
         selected-language (cursor state [:content-language])
         {:keys [name description tags criteria_content image_file image_file issuer_content_name issuer_content_url issuer_contact issuer_image issuer_description criteria_url  creator_name creator_url creator_email creator_image creator_description message_count]} (content-setter @selected-language content)
         tags (tag-parser tags)]
-    [:div {:id "badge-contents"}
-     [:div {:class "pull-right text-right"}
-             [follow-badge badge_id]]
-       (if (or verified_by_obf issued_by_obf)
-         (bh/issued-by-obf obf_url verified_by_obf issued_by_obf))
-       [:div.row
-        [:div {:class "col-md-3 badge-image modal-left"}
-         [:img {:src (str "/" image_file)}]
-         (when (> average_rating 0)
-           [:div.rating
-            [r/rate-it average_rating]
-            [:div (if (= rating_count 1)
-                    (str (t :gallery/Ratedby) " " (t :gallery/oneearner))
-                    (str (t :gallery/Ratedby) " " rating_count " " (t :gallery/earners)))]])
-         [:div
-          [gallery-modal-message-info-link show-messages badge_id]
-          ]]
-        [:div {:class "col-md-9 badge-info"}
-         (content-language-selector selected-language content)
-         (if @show-messages
-           [:div.rowmessage
-            [:h1.uppercase-header (str name " - " (t :social/Messages))]
-            [badge-message-handler badge_id reload-fn]]
-            
-           [:div.rowcontent
-            [:h1.uppercase-header name]
-            [:div
-             (bh/issuer-label-image-link issuer_content_name issuer_content_url issuer_contact issuer_image)
-             (bh/creator-label-image-link creator_name creator_url creator_email creator_image)
-             [:div.row
-              [:div {:class "col-md-12 description"}
-               description]]
-             [:div.row
-              [:div {:class "col-md-12 badge-info"}
-                  [:h2.uppercase-header (t :badge/Criteria)]
-               [:div.row
-                [:div.col-md-12
-                 [:a {:href   criteria_url
-                      :target "_blank"} (t :badge/Opencriteriapage)]]]
-                  [:div.row
-                   [:div.col-md-12
-                    {:dangerouslySetInnerHTML {:__html criteria_content}}]]]]
-             ]
-            (if (not (empty? tags))
+    [:div
+     [:div.pull-right
+       [follow-badge badge_id]]
+     [:div {:id "badge-contents"}
+      [:div {:class "pull-right text-right"}]
+      (if (or verified_by_obf issued_by_obf)
+        (bh/issued-by-obf obf_url verified_by_obf issued_by_obf))
+      [:div.row
+       [:div {:class "col-md-3 badge-image modal-left"}
+        [:img {:src (str "/" image_file)}]
+        (when (> average_rating 0)
+          [:div.rating
+           [r/rate-it average_rating]
+           [:div (if (= rating_count 1)
+                   (str (t :gallery/Ratedby) " " (t :gallery/oneearner))
+                   (str (t :gallery/Ratedby) " " rating_count " " (t :gallery/earners)))]])
+        [:div
+         [gallery-modal-message-info-link show-messages badge_id]
+         ]]
+       [:div {:class "col-md-9 badge-info"}
+        (content-language-selector selected-language content)
+        (if @show-messages
+          [:div.rowmessage
+           [:h1.uppercase-header (str name " - " (t :social/Messages))]
+           [badge-message-handler badge_id reload-fn]]
+          
+          [:div.rowcontent
+           [:h1.uppercase-header name]
+           [:div
+            (bh/issuer-label-image-link issuer_content_name issuer_content_url issuer_contact issuer_image)
+            (bh/creator-label-image-link creator_name creator_url creator_email creator_image)
+            [:div.row
+             [:div {:class "col-md-12 description"}
+              description]]
+            [:div.row
+             [:div {:class "col-md-12 badge-info"}
+              [:h2.uppercase-header (t :badge/Criteria)]
+              [:div.row
+               [:div.col-md-12
+                [:a {:href   criteria_url
+                     :target "_blank"} (t :badge/Opencriteriapage)]]]
+              [:div.row
+               [:div.col-md-12
+                {:dangerouslySetInnerHTML {:__html criteria_content}}]]]]
+            ]
+           (if (not (empty? tags))
              (into [:div]
                    (for [tag tags]
                      [:p {:id "tag"}
                       (str "#" tag )])))
-            (if (or (> (count public_users) 0) (> private_user_count 0))
-              [:div.recipients
-               [:div
+           (if (or (> (count public_users) 0) (> private_user_count 0))
+             [:div.recipients
+              [:div
 
-                [:h2.uppercase-header (t :gallery/Allrecipients)]]
-               [:div
-                (into [:div]
-                      (for [user public_users
-                            :let [{:keys [id first_name last_name profile_picture]} user]]
-                        (profile-link-inline-modal id first_name last_name profile_picture)))
-                (if (> private_user_count 0)
-                  (if (> (count public_users) 0)
-                    [:span "... " (t :core/and) " " private_user_count " " (t :core/more)]
-                    [:span private_user_count " " (if (> private_user_count 1) (t :gallery/recipients) (t :gallery/recipient))]))]])])
-         ]]
-     [reporttool1 badge_id name "badges"]]
+               [:h2.uppercase-header (t :gallery/Allrecipients)]]
+              [:div
+               (into [:div]
+                     (for [user public_users
+                           :let [{:keys [id first_name last_name profile_picture]} user]]
+                       (profile-link-inline-modal id first_name last_name profile_picture)))
+               (if (> private_user_count 0)
+                 (if (> (count public_users) 0)
+                   [:span "... " (t :core/and) " " private_user_count " " (t :core/more)]
+                   [:span private_user_count " " (if (> private_user_count 1) (t :gallery/recipients) (t :gallery/recipient))]))]])])
+        ]]
+      [reporttool1 badge_id name "badges"]]]
     ))
 
 
