176bdba31041ef599854055afa442cbef1c73158
Antti Koskinen <antti.koskinen@discendum.com>
2018-02-15 10:01:49 +0200

    New version of org.clojure/tools.nrepl

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
 
                  [com.stuartsierra/component "0.3.2"]
-                 [org.clojure/tools.nrepl "0.2.12"]
+                 [org.clojure/tools.nrepl "0.2.13"]
 
                  ; Dependecy conflict overrides
                  [org.clojure/tools.reader "1.0.0-beta3"]
