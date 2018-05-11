1f0ca7e8e336ffd82507b4614526db5d65d1e21f
isaakay111 <ogunlolui@gmail.com>
2018-05-11 09:56:29 +0300

    user-terms table edit

--- resources/migrations/user/sql/201805110954-user-terms.up.sql
+++ resources/migrations/user/sql/201805110954-user-terms.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `user_terms` (
   `user_id` bigint(20) NOT NULL,
   `status` enum('accepted','declined') DEFAULT 'declined',
+  `ctime` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
