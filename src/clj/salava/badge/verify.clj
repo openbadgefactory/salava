8d5c9398f1d75c91cc972e71be7c25ce962a6e51
isaakay111 <ogunlolui@gmail.com>
2018-07-31 17:22:48 +0300

    verify badge logic

--- src/clj/salava/badge/verify.clj
+++ src/clj/salava/badge/verify.clj
@@ -1,37 +1,68 @@
 (ns salava.badge.verify
   (:require [salava.badge.main :as b]
             [salava.core.http :as http]
-            [salava.core.time :refer [iso8601-to-unix-time unix-time]]))
+            [salava.core.time :refer [iso8601-to-unix-time unix-time]]
+            [clojure.tools.logging :as log]
+            #_[clj-http.client :as client]))
+
+(defn url? [s]
+  (not (clojure.string/blank? (re-find #"^http" (str s)))))
 
 (defn fetch-json-data [url]
-  #_(log/info "fetch-json-data: GET" url)
-  (http/http-get url {:as :json :accept :json :throw-entire-message? true}))
+  (log/info "fetch-badge-data: GET" url)
+  (http/http-get url {:as :json :accept :json :throw-entire-message? false}))
+
+(defn fetch-image [url]
+  (log/info "fetch image from " url)
+  (try
+    (http/http-req {:url url :method :get :throw-exceptions false :as :byte-array})
+    (catch Exception e
+      (log/error "failed to get image from " url " Message: " (.getMessage e))
+      {:status 500})))
+
+(defn fetch-url [url]
+  (log/info "fetch response from " url)
+  (try
+    (http/http-req {:url url :method :get :throw-exceptions false})
+    (catch Exception e
+      (log/error "failed to get response from " url " Message: " (.getMessage e))
+      {:status 500})))
+
 
 (defn- assertion [assertion-url]
-  (http/http-req {:socket-timeout 10000
-                  :conn-timeout   10000
-                  :method :get
-                  :url assertion-url
-                  :throw-exceptions false
-                  :as :json
-                  :accept :json}))
+  (try
+    (http/http-req  {:url assertion-url :as :json :method :get  :accept :json :throw-exceptions false })
+    (catch Exception e
+      (log/error "failed to get assertion url " assertion-url " Message: "(.getMessage e) )
+      {:status 500})))
 
-(defn verify-badge [ctx badge-id user-id]
-  (let [badge (b/get-badge ctx badge-id user-id)
-        assertion-url (:assertion_url badge)
-        asr-response (assertion assertion-url)
-        asr-data (:body asr-response)
-        assertion-badge-url (:badge asr-data)
-        badge-data (http/http-get assertion-badge-url {:as :json :accept :json :throw-entire-message? true})
-        ;criteria (if (map? (:criteria badge-data)) (fetch-json-data (get-in badge-data [:criteria :id])) (fetch-json-data (:criteria badge-data)))
-        revoked? (or (= (:status asr-response) 410) (:revoked asr-data))
-        expired? (and (:expires asr-data) (< (iso8601-to-unix-time (:expires asr-data)) (unix-time)))]
-   (prn (:criteria badge-data))
-  {:revoked revoked?
-   :expired expired?
-   :badge {
-            }
-   }
+(defn verify-badge [ctx asr user-id]
+  (log/info "Badge verification initiated:")
+  (let [asr-response (assertion asr)
+        result {}]
+    (case (:status asr-response)
+      410 (assoc result
+            :assertion-status 410
+            :revoked? true)
+      500 (assoc result :assertion-status 500
+            :badge-status "Broken url, badge can't be verified")
+      200 (let [asr-data (:body asr-response)
+                badge-data (fetch-json-data (:badge asr-data))
+                badge-image (if (map? (:image badge-data)) (fetch-image (get-in badge-data [:image :id])) (fetch-image (:image badge-data)))
+                badge-criteria (if (and (map? (:criteria badge-data))(contains? (:criteria badge-data) :id))
+                                 (fetch-url (get-in badge-data [:criteria :id]))
+                                 (if (url? (:criteria badge-data)) (fetch-url (:criteria badge-data)) {:status 800}))
+                badge-issuer (if (and (map? (:issuer badge-data)) (contains? (:issuer badge-data) :id))
+                               (fetch-url (get-in badge-data [:issuer :id]))
+                               (if (url? (:issuer badge-data)) (fetch-url (:issuer badge-data)) {:status 800}))
+                revoked? (or (= (:status asr-response) 410) (:revoked asr-data))
+                expired? (and (:expires asr-data) (< (iso8601-to-unix-time (:expires asr-data)) (unix-time)))]
 
-    )
-)
+            (assoc result :assertion-status 200
+              :assertion asr-data
+              :badge-image-status (:status badge-image)
+              :badge-criteria-status (:status badge-criteria)
+              :badge-issuer-status (:status badge-issuer)
+              :revoked? revoked?
+              :expired? expired?
+              )))))
