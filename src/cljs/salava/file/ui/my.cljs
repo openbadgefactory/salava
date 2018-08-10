169b383fbbfc7638b201fe037552c6ff0842c56d
isaakay111 <ogunlolui@gmail.com>
2018-08-10 17:23:31 +0300

    fix files overflow

--- src/cljs/salava/file/ui/my.cljs
+++ src/cljs/salava/file/ui/my.cljs
@@ -168,7 +168,7 @@
         max-sizetext (if (not-empty max-size)
                        (str "(" (t :file/Maxfilesize) ": "  max-size ")")
                        "")]
-    [:div {:class "row flip"
+    [:div {:class "row wrap-grid"
            :id    "grid"}
      [:div {:class "col-xs-12 col-sm-6 col-md-4"
             :id "add-element"
