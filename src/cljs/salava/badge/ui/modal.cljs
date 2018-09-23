8626688afdbe9375c172a5fe9a617361b2b5b5c1
isaakay111 <ogunlolui@gmail.com>
2018-09-23 14:16:52 +0300

    refactor badge content

--- src/cljs/salava/badge/ui/modal.cljs
+++ src/cljs/salava/badge/ui/modal.cljs
@@ -1,32 +1,24 @@
 (ns salava.badge.ui.modal
   (:require [reagent.core :refer [atom cursor]]
             [reagent.session :as session]
-            [reagent-modals.modals :as m]
             [salava.core.ui.ajax-utils :as ajax]
-            [salava.core.ui.layout :as layout]
             [salava.core.i18n :refer [t]]
             [salava.badge.ui.helper :as bh]
             [salava.badge.ui.assertion :as a]
             [salava.badge.ui.settings :as se]
-            [salava.core.ui.rate-it :as r]
             [salava.core.ui.share :as s]
-            [salava.core.helper :refer [dump]]
-            [salava.user.ui.helper :as uh]
-            [salava.gallery.ui.badges :as b]
-            [salava.core.ui.helper :refer [path-for private?]]
-            [salava.core.time :refer [date-from-unix-time unix-time]]
-            [salava.admin.ui.admintool :refer [admintool]]
+            [salava.core.ui.helper :refer [path-for]]
+            [salava.core.time :refer [date-from-unix-time]]
             [salava.social.ui.follow :refer [follow-badge]]
             [salava.core.ui.error :as err]
             [salava.core.ui.modal :as mo]
             [salava.core.ui.content-language :refer [init-content-language content-language-selector content-setter]]
-            ;[salava.badge.ui.endorsement :refer [endorsement-modal-link]]
             [salava.badge.ui.endorsement :as endr]
             [salava.badge.ui.issuer :as issuer]
             [salava.social.ui.badge-message-modal :refer [badge-message-link]]
             [salava.admin.ui.reporttool :refer [reporttool1]]
-            [salava.badge.ui.verify :refer [check-badge]])
-  )
+            [salava.badge.ui.verify :refer [check-badge]]
+            [salava.core.ui.tag :as tag]))
 
 
 
@@ -67,109 +59,99 @@
              :on-click #(do (.preventDefault %)
                           (mo/open-modal [:badge :creator] creator-id))} name]]]))
 
-#_(defn verify-badge-link [id]
-  [:div {:id "verify-link"}
-   [:a.link {:href "#"
-             :on-click #(do (.preventDefault %)
-                          (mo/open-modal [:badge :verify] id))}
-    (str (t :badge/Verifybadge) "...")]])
-
-
-
-(defn content [state]
-
+(defn badge-content [state]
   (let [{:keys [id badge_id  owner? visibility show_evidence rating issuer_image issued_on expires_on revoked issuer_content_id issuer_content_name issuer_content_url issuer_contact issuer_description first_name last_name description criteria_url criteria_content user-logged-in? congratulated? congratulations view_count evidence_url issued_by_obf verified_by_obf obf_url recipient_count assertion creator_content_id creator_name creator_image creator_url creator_email creator_description  qr_code owner message_count issuer-endorsements content endorsement_count endorsements]} @state
         expired? (bh/badge-expired? expires_on)
         show-recipient-name-atom (cursor state [:show_recipient_name])
         selected-language (cursor state [:content-language])
         {:keys [name description tags alignment criteria_content image_file image_file issuer_content_id issuer_content_name issuer_content_url issuer_contact issuer_image issuer_description criteria_url  creator_name creator_url creator_email creator_image creator_description message_count endorsement_count]} (content-setter @selected-language content)]
+    [:div {:id "badge-info" :class "row flip"}
+     [:div {:class "col-md-3"}
+      [:div.badge-image
+      [:img {:src (str "/" image_file)}]]
+      [below-image-block state endorsement_count]]
+     [:div {:class "col-md-9 badge-info view-tab"}
+      [:div.row
+       [:div {:class "col-md-12"}
+        [:h1.uppercase-header name]
+        (if (< 1 (count (:content @state)))
+          [:div.inline [:label (t :core/Languages)": "](content-language-selector selected-language (:content @state))])
+        (issuer-modal-link issuer_content_id issuer_content_name)
+        (creator-modal-link creator_content_id creator_name)
 
-    [:div
-     [:div.col-xs-12
-      [:div.pull-right
-       [follow-badge badge_id]]]
-     [:div {:id "badge-info"}
-      [:div.panel
-       [:div.panel-body
-        (if (or verified_by_obf issued_by_obf)
-          (bh/issued-by-obf obf_url verified_by_obf issued_by_obf))
+        (if (and issued_on (> issued_on 0))
+          [:div [:label (t :badge/Issuedon) ": "]  (date-from-unix-time (* 1000 issued_on))])
+        (if (and expires_on (not expired?))
+          [:div [:label (t :badge/Expiresin) ": "]  (str (num-days-left expires_on) " " (t :badge/days))#_(date-from-unix-time (* 1000 expires_on))])
+
+        (if (pos? @show-recipient-name-atom)
+          (if (and user-logged-in? (not owner?))
+            [:div [:label (t :badge/Recipient) ": " ] [:a {:href (path-for (str "/user/profile/" owner))} first_name " " last_name]]
+            [:div [:label (t :badge/Recipient) ": "]  first_name " " last_name]))
+
+        [:div.description description]
+
+        ;check-badge
+        (check-badge id)]]
+
+      (when-not (empty? alignment)
         [:div.row
-         [:div {:class "col-md-3 badge-image"}
-          [:div.row
-           [:div.col-xs-12
-            [:img {:src (str "/" image_file)}]]]
-          [:div.row
-           [:div.col-xs-12 {:id "badge-congratulated"}
-            (if (and user-logged-in? (not owner?))
-              (if congratulated?
-                [:div.congratulated
-                 [:i {:class "fa fa-heart"}]
-                 (str " " (t :badge/Congratulated))]
-                [:button {:class    "btn btn-primary"
-                          :on-click #(congratulate state)}
-                 [:i {:class "fa fa-heart"}]
-                 (str " " (t :badge/Congratulate) "!")])
-              )]]
-          #_(if (session/get :user)
-              [badge-message-link message_count  badge_id])
-          ]
-         [:div {:class "col-md-9 badge-info"}
-          [:div.row
-           [:div {:class "col-md-12"}
-            (content-language-selector selected-language (:content @state))
+         [:div.col-md-12
+          [:h2.uppercase-header (t :badge/Alignments)]
+          (doall
+            (map (fn [{:keys [name url description]}]
+                   [:p {:key url}
+                    [:a {:target "_blank" :rel "noopener noreferrer" :href url} name] [:br] description])
+                 alignment))]])
 
-            [:h1.uppercase-header name]
-            #_(bh/issuer-label-image-link issuer_content_name issuer_content_url issuer_description issuer_contact issuer_image issuer-endorsements)
-            #_(bh/creator-label-image-link creator_name creator_url creator_description creator_email creator_image)
-            (issuer-modal-link issuer_content_id issuer_content_name)
-            (creator-modal-link creator_content_id creator_name)
+      [:div {:class "row criteria-html"}
+       [:div.col-md-12
+        [:h2.uppercase-header (t :badge/Criteria)]
+        [:a {:href criteria_url :target "_blank"} (t :badge/Opencriteriapage) "..."]
+        [:div {:dangerouslySetInnerHTML {:__html criteria_content}}]]]
 
-            (if (and issued_on (> issued_on 0))
-              [:div [:label (t :badge/Issuedon) ": "]  (date-from-unix-time (* 1000 issued_on))])
-            (if (and expires_on (not expired?))
-              [:div [:label (t :badge/Expireson) ": "]  (date-from-unix-time (* 1000 expires_on))])
-            #_(if assertion
-              [:div {:id "assertion-link"}
-               [:label (t :badge/Metadata)": "]
-               [:a {:href     "#"
-                    :on-click #(mo/set-new-view [:badge :metadata] (dissoc assertion :evidence))}
-                (t :badge/Openassertion) "..."]])
+      (if (and (pos? show_evidence) evidence_url)
+        [:div.row
+         [:div.col-md-12
+          [:h2.uppercase-header (t :badge/Evidence)]
+          [:div [:a {:target "_blank" :href evidence_url} (t :badge/Openevidencepage) "..."]]]])
 
-            (if (pos? @show-recipient-name-atom)
-              (if (and user-logged-in? (not owner?))
-                [:div [:label (t :badge/Recipient) ": " ] [:a {:href (path-for (str "/user/profile/" owner))} first_name " " last_name]]
-                [:div [:label (t :badge/Recipient) ": "]  first_name " " last_name])
-              )
+      (if owner? "" [reporttool1 id name "badge"])]]
+    ))
 
-            [:div.description description]
+(defn modal-navi [state]
+  (let [selected-language (cursor state [:content-language])
+        data (content-setter @selected-language (:content @state))]
+    [:div.col-md-9.badge-modal-navi
+     [:ul {:class "nav nav-tabs wrap-grid"}
+      [:li.nav-item{:class (if (or (nil? (:tab-no @state))(= 1 (:tab-no @state))) "active")}
+       [:a.nav-link {:href "#" :on-click #(swap! state assoc :tab [badge-content state] :tab-no 1 )}
+        [:div  [:i.nav-icon {:class "fa fa-eye fa-lg"}] (t :page/View)  ]]]
+      [:li.nav-item {:class (if (= 2 (:tab-no @state)) "active")}
+       [:a.nav-link {:href "#" :on-click #(show-settings-dialog (:id @state) state init-data "settings")}
+        [:div  [:i.nav-icon {:class "fa fa-cogs fa-lg"}] (t :page/Settings)  ]]]
+      [:li.nav-item {:class (if (= 3 (:tab-no @state)) "active")}
+       [:a.nav-link {:href "#" :on-click #(show-settings-dialog (:id @state) state init-data "share")}
+        [:div  [:i.nav-icon {:class "fa fa-share-alt fa-lg"}] (t :badge/Share)  ]]]
+      [:li.nav-item {:class (if (= 4 (:tab-no @state)) "active")}
+       [:a.nav-link {:href "#" :on-click #(swap! state assoc :tab [se/download-tab-content (assoc data :assertion_url (:assertion_url @state)
+                                                                                                       :obf_url (:obf_url @state)) state] :tab-no 4)}
+        [:div  [:i.nav-icon {:class "fa fa-download fa-lg"}] (t :core/Download)  ]]]
+      [:li.nav-item {:class (if (= 5 (:tab-no @state)) "active")}
+       [:a.nav-link.delete-button {:href "#" :on-click #(do
+                                                          (swap! state assoc-in [:badge-settings :confirm-delete?] true)
+                                                          (swap! state assoc :tab [se/delete-tab-content data state] :tab-no 5))}
+        [:i.nav-icon {:class "fa fa-trash fa-lg"}] (t :core/Delete) ]]]]))
 
-            ;check-badge
-            (check-badge id)]]
 
-          (when-not (empty? alignment)
-            [:div.row
-             [:div.col-md-12
-              [:h2.uppercase-header (t :badge/Alignments)]
-              (doall
-                (map (fn [{:keys [name url description]}]
-                       [:p {:key url}
-                        [:a {:target "_blank" :rel "noopener noreferrer" :href url} name] [:br] description])
-                     alignment))]])
-
-          [:div {:class "row criteria-html"}
-           [:div.col-md-12
-            [:h2.uppercase-header (t :badge/Criteria)]
-            [:a {:href criteria_url :target "_blank"} (t :badge/Opencriteriapage) "..."]
-            [:div {:dangerouslySetInnerHTML {:__html criteria_content}}]]]
-
-          (if (and (pos? show_evidence) evidence_url)
-            [:div.row
-             [:div.col-md-12
-              [:h2.uppercase-header (t :badge/Evidence)]
-              [:div [:a {:target "_blank" :href evidence_url} (t :badge/Openevidencepage) "..."]]]])
-
-          ]]
-        (if owner? "" [reporttool1 id name "badge"])]]]]
+(defn modal-top-bar [state]
+  (let [{:keys [verified_by_obf issued_by_obf obf_url owner?]} @state]
+    [:div.row.flip
+     (if (or verified_by_obf issued_by_obf)
+       (bh/issued-by-obf obf_url verified_by_obf issued_by_obf)
+       [:div.col-md-3])
+     (if owner? [modal-navi state])
+     ]
     ))
 
 
