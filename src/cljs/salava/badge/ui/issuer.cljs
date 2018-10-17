3b53f6870b6f5c0044fcd3c502ed3a608c83d8ff
isaakay111 <ogunlolui@gmail.com>
2018-10-17 14:02:48 +0300

    issuer connections

--- src/cljs/salava/badge/ui/issuer.cljs
+++ src/cljs/salava/badge/ui/issuer.cljs
@@ -1,5 +1,5 @@
 (ns salava.badge.ui.issuer
-  (:require [reagent.core :refer [atom]]
+  (:require [reagent.core :refer [atom cursor]]
             [salava.core.i18n :refer [t]]
             [salava.core.ui.ajax-utils :as ajax]
             [salava.core.ui.helper :refer [path-for private?]]
@@ -7,17 +7,40 @@
 
 
 
+
+(defn init-issuer-connection [issuer-id state]
+  (ajax/GET
+    (path-for (str "/obpv1/social/issuer_connected/" issuer-id))
+    {:handler (fn [data]
+                (swap! state assoc :connected data)
+                )}))
+
 (defn init-issuer-content [state issuer-id]
   (ajax/GET
     (path-for (str "/obpv1/badge/issuer/" issuer-id))
-    {:handler (fn [data] (reset! state data))}))
+    {:handler (fn [data]
+                (reset! state data)
+                (init-issuer-connection issuer-id state))
+     }))
 
 (defn init-creator-content [state creator-id]
   (ajax/GET
     (path-for (str "/obpv1/badge/creator/" creator-id))
     {:handler (fn [data]
-                (prn data)
                 (reset! state data))}))
+
+(defn add-issuer-to-favourites [issuer-id state]
+  (ajax/POST
+    (path-for (str "/obpv1/social/create_connection_issuer/" issuer-id))
+    {:handler (fn []
+                (init-issuer-connection issuer-id state))}))
+
+
+(defn remove-issuer-from-favourites [issuer-id state]
+  (ajax/POST
+    (path-for (str "/obpv1/social/delete_connection_issuer/" issuer-id))
+    {:handler (fn []
+                (init-issuer-connection issuer-id state))}))
 
 (defn- issuer-image [path]
   (when (not-empty path)
@@ -26,12 +49,21 @@
       :style {:width "50px"}}]))
 
 (defn content [issuer-id]
-  (let [state (atom {:issuer nil :endorsement []})]
+  (let [state (atom {:issuer nil :endorsement []})
+        connected? (cursor state [:connected])]
     (init-issuer-content state issuer-id)
     (fn []
       (let [{:keys [name description email url image_file]} @state]
         [:div.row {:id "badge-contents"}
          [:div.col-xs-12
+          [:div.row
+           ;TODO users not logged in don't see this
+           [:div.pull-right
+            (if-not @connected?
+              [:a {:href "#" :on-click #(add-issuer-to-favourites issuer-id state)} [:i {:class "fa fa-bookmark-o"}] " add to favourites"]
+              [:a {:href "#" :on-click #(remove-issuer-from-favourites issuer-id state)} [:i {:class "fa fa-bookmark"}] " remove from favourites"]
+              )]
+           ]
           [:h2.uppercase-header
            (issuer-image image_file)
            " "
