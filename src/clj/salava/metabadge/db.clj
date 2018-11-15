136f2239ff4715d468a7df8b215fe72b179eb7d8
isaakay111 <ogunlolui@gmail.com>
2018-11-15 14:45:09 +0200

    refactor

--- src/clj/salava/metabadge/db.clj
+++ src/clj/salava/metabadge/db.clj
@@ -7,4 +7,4 @@
 (defn badge-id-by-assertion
   "in case of multiple hits return the most recent"
   [ctx assertion_url]
-  (some-> (select-user-badge-id-by-assertion-url {:assertion_url assertion_url} (u/get-db ctx)) #_(into {:result-set-fn first :row-fn :id} (u/get-db ctx)) first :id))
+  (some-> (select-user-badge-id-by-assertion-url {:assertion_url assertion_url} (u/get-db ctx)) first :id))
