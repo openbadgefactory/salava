97cbd7a4e944469e362c85fd63e4131900745c15
isaakay111 <ogunlolui@gmail.com>
2018-11-19 16:29:48 +0200

    build metabadges from user badges

--- src/clj/salava/metabadge/metabadge.clj
+++ src/clj/salava/metabadge/metabadge.clj
@@ -10,7 +10,9 @@
             [salava.metabadge.db :as db]
             [clojure.string :as string]
             [salava.badge.main :as b]
-            [salava.core.helper :refer [dump]]))
+            [salava.core.helper :refer [dump]]
+            [clojure.set :as s]
+            [medley.core :refer [distinct-by]]))
 
 (def no-of-days-in-cache 30)
 
@@ -18,6 +20,10 @@
 
 (defn url? [s]
   (not (clojure.string/blank? (re-find #"^http" (str s)))))
+
+(defn %completed [req gotten]
+  (let [calc (Math/round (double (* (/ gotten req) 100)))]
+    (if (> calc 100) 100 calc)))
 
 (defn- file-extension [filename]
   (try+
@@ -30,8 +36,6 @@
   (try
     (http/http-get url {:as :json :accept :json :throw-entire-message? true})
     (catch Exception e
-      ;(log/error "could not fetch-json-data: " url)
-      ;(log/error (.getMessage e))
       {})))
 
 (defn fetch-image [ctx url]
@@ -49,8 +53,7 @@
       (if-let [data (fetch-json-data url)]
         (if (url? (:image data))
           (-> data (assoc :image (fetch-image ctx (:image data))))
-          data)
-        )
+          data))
       (catch Exception _
         (log/error (str "Did not get required badge information: " id))
         {}))))
@@ -64,13 +67,7 @@
                             (cache/hit % key)
                             (cache/miss % key (public-badge-info ctx id)))) key)))
 
-#_(defn- expand-required-badges [ctx badges assertion-url]
-    (r/reduce (fn [result b]
-                (if-let [badge-info (get-badge-data ctx b)]
-                  (conj result (-> b (assoc :badge-info badge-info
-                                       :current (= (:url b) assertion-url)))))) [] badges))
-
-(defn- expand-required-badges [ctx badges assertion-url] ;;experiment with reducers -> improved speed
+(defn- expand-required-badges [ctx badges assertion-url]
   (->> badges
        (r/map (fn [b] (-> b (assoc :badge-info (get-badge-data ctx b)
                               :current (= (:url b) assertion-url)
@@ -78,18 +75,7 @@
        (r/foldcat)))
 
 
-#_(defn- process-metabadge [ctx metabadge assertion-url]
-    (let [content (:metabadge metabadge)]
-      (assoc metabadge :metabadge
-        (r/reduce
-          (fn [result m]
-            (conj result (-> m
-                             (assoc :required_badges (expand-required-badges ctx (:required_badges m) assertion-url) :milestone? (= assertion-url (get-in m [:badge :url])))
-                             (update-in [:badge] merge (get-badge-data ctx (:badge m)) ))))
-          []
-          content))))
-
-(defn- process-metabadge [ctx metabadge assertion-url] ;;experiment with reducers
+(defn- process-metabadge [ctx metabadge assertion-url]
   (->> (:metabadge metabadge)
        (r/map (fn [m] (-> m
                           (assoc :required_badges (expand-required-badges ctx (:required_badges m) assertion-url)
@@ -110,7 +96,9 @@
         (log/error (.getMessage e))
         nil))))
 
-(defn quick-check-metabadge [ctx assertion-url]
+(defn quick-check-metabadge
+  "check if badge is metabadge without processing"
+  [ctx assertion-url]
   (let [meta-data-url (str (get-in ctx [:config :factory :url]) "/v1/assertion/metabadge/?url=" (u/url-encode assertion-url))]
     (if-let [metabadge (fetch-json-data meta-data-url)]
       (if (empty? metabadge)
@@ -120,7 +108,9 @@
 
       nil)))
 
-(defn milestone? "check if badge is a milestone badge, a required badge or both" [ctx assertion-url]
+(defn milestone?
+  "check if badge is a milestone badge, a required badge or both"
+  [ctx assertion-url]
   (if-let [check (string/starts-with? assertion-url (get-in ctx [:config :factory :url]))]
     (let [metabadge (quick-check-metabadge ctx assertion-url)]
       (r/reduce (fn [r k _]
@@ -129,15 +119,25 @@
     {}))
 
 
-(defn all-metabadges [ctx user]
+(defn all-metabadges
+  "build metabadges from user badges"
+  [ctx user]
   (let [obf-url (get-in ctx [:config :factory :url])
         all-badges (filter (fn [b] (if-not (:assertion_url b) false (-> b :assertion_url (string/starts-with? obf-url)) ))(b/user-badges-all ctx (:id user)))
         metabadges (r/reduce (fn [r m ]
                                (if-let [check (string/starts-with? (:assertion_url m) obf-url)]
                                  (conj r  (check-metabadge ctx (:assertion_url m)))
                                  )) [] all-badges)]
-      metabadges
-     #_(mapv #(vals %) metabadges)))
+
+    (reduce-kv
+      (fn [r k v] (distinct-by :id (into [] (r/flatten (conj r  (->> v :metabadge
+                                                                     (map (fn [m]
+                                                                            (let [amount_received (count (filter :received (-> m :required_badges)))]
+                                                                              (assoc m :completion_status (%completed (-> m :min_required ) amount_received)))
+                                                                            ))
+                                                                     (map #(identity %))))))))
+      []
+      metabadges)))
 
 
 
