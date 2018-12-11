a2204c46688c67ade2b4c0d95ae47012ba1b1972
isaakay111 <ogunlolui@gmail.com>
2018-12-11 17:20:23 +0200

    fix migrations

--- resources/migrations/metabadge/sql/201812101426-metabadge-tables.down.sql
+++ resources/migrations/metabadge/sql/201812101426-metabadge-tables.down.sql
@@ -7,3 +7,11 @@ DROP TABLE IF EXISTS `factory_metabadge_required`;
 --;;
 
 DROP TABLE IF EXISTS `user_badge_metabadge`;
+
+--;;
+
+DROP TABLE IF EXISTS `user_metabadge_received`;
+
+--;;
+
+DROP TABLE IF EXISTS `user_metabadge_required_received`;
--- resources/migrations/metabadge/sql/201812101426-metabadge-tables.up.sql
+++ resources/migrations/metabadge/sql/201812101426-metabadge-tables.up.sql
@@ -31,5 +31,29 @@ CREATE TABLE `user_badge_metabadge` (
   `user_badge_id` bigint(20) unsigned NOT NULL,
   `meta_badge` varchar(255) DEFAULT NULL,
   `meta_badge_req` varchar(255) DEFAULT NULL,
+  `last_modified` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`user_badge_id`, `meta_badge`, `meta_badge_req`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--;;
+
+CREATE TABLE `user_metabadge_received` (
+  `metabadge_id` varchar(255) NOT NULL,
+  `user_badge_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `name` text,
+  `min_required` bigint(20) unsigned DEFAULT 0,
+  `last_modified` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`user_badge_id`, `metabadge_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--;;
+
+CREATE TABLE `user_metabadge_required_received` (
+  `metabadge_id` varchar(255) NOT NULL,
+  `user_metabadge_id` varchar(255) NOT NULL,
+  `user_required_badge_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `last_modified` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`metabadge_id`, `user_required_badge_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
