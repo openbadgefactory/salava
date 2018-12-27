5b18fbf9a0cfa70c9d050845f6016f523cfd535e
isaakay111 <ogunlolui@gmail.com>
2018-12-27 13:22:05 +0200

    fix metabadge migrations

--- resources/migrations/metabadge/sql/201812271319-metabadge-tables.down.sql
+++ resources/migrations/metabadge/sql/201812271319-metabadge-tables.down.sql
@@ -7,11 +7,3 @@ DROP TABLE IF EXISTS `factory_metabadge_required`;
 --;;
 
 DROP TABLE IF EXISTS `user_badge_metabadge`;
-
---;;
-
-DROP TABLE IF EXISTS `user_metabadge_received`;
-
---;;
-
-DROP TABLE IF EXISTS `user_metabadge_required_received`;
