357e550b6d828ee76f21b3b490d648ed4bd82d96
isaakay111 <ogunlolui@gmail.com>
2019-01-18 16:23:53 +0200

    evidence

--- src/cljs/salava/badge/ui/evidence.cljs
+++ src/cljs/salava/badge/ui/evidence.cljs
@@ -58,7 +58,10 @@
                 nil)
         evidence-url-atom (cursor state [:evidence :url])
         show-url? (cursor state [:evidence :show_url])
-        resource-input-mode (cursor state [:input_mode])]
+        resource-input-mode (cursor state [:input_mode])
+        update-page-visibility ()]
+    (prn visibility)
+   (when (= "private" visibility) (swap! state assoc :evidence {:update-page-visibility id}))
     [:div.radio
      [:label
       [:input { :name "opt"
@@ -126,7 +129,7 @@
         {:keys[files]} @resource-atom]
     [:div.resource-upload
      (if files
-       [:div.col-md-9 [:label
+       [:div [:label
                        [:span [:i.fa.fa-upload] (t :file/Upload) ]
                        [:input {:id "grid-file-upload"
                                 :type "file"
@@ -142,7 +145,8 @@
 
 
 (defn init-evidence-form [evidence state show-url?]
-  (let [evidence { :id (:id evidence)
+  (let [ {:keys [id name url audience narrative genre description]} evidence
+         evidence { :id (:id evidence)
                    :name (:name evidence)
                    :url (:url evidence)
                    :audience (:audience evidence)
@@ -150,13 +154,15 @@
                    :genre (:genre evidence)
                    :description (:description evidence)
                    }]
+    (if (every? #(blank? %) (vector name audience narrative genre description))
+      (swap! state assoc :show_form nil)
+      (swap! state assoc :show_form true))
     (if show-url?
-      (swap! state assoc :evidence (assoc evidence :show_url true
-                                     :input_mode nil) )
+      (swap! state assoc :evidence (assoc evidence :show_url true :input_mode nil) )
       (swap! state assoc :evidence evidence))))
 
 (defn save-badge-evidence [data state init-data]
-  (let [{:keys [id name description audience genre url narrative]} (:evidence @state)
+  (let [{:keys [id name description url update-page-visibility]} (:evidence @state)
         badge-id (:id data)
         init-settings (first (plugin-fun (session/get :plugins) "modal" "show_settings_dialog"))]
     (swap! state assoc :evidence {:message nil})
@@ -168,14 +174,10 @@
         {:params {:id id
                   :name (if name (trim name))
                   :description description
-                  :audience (if audience (trim audience))
-                  :genre (if genre (trim genre))
-                  :url (if url (trim url))
-                  :narrative narrative}
+                  :url (if url (trim url))}
          :handler (fn [resp]
                     (when (= "success" (:status resp))
-                      (init-data state (:id data) nil)
-                      #_(init-settings (:id data) state init-data "settings"))
+                      (init-settings (:id data) state init-data "settings"))
 
 
                     )}))))
@@ -219,7 +221,7 @@
           :page_input (do
                         (init-resources :page_input resource-atom)
                         [:div [resources-grid :page_input resource-atom state]])
-          :url_input [:div
+          :url_input [:div {:style {:margin-bottom "10px"}}
                       [input {:name "evidence-url" :atom evidence-url-atom :type "url"}]
                       [:button {:type "button"
                                 :class "btn btn-primary"
@@ -227,9 +229,8 @@
                                 :on-click #(do
                                              (.preventDefault %)
                                              (reset! show-url? true)
-                                             (reset! resource-input-mode nil)
-                                             #_(swap! state assoc-in [:evidence :url] @evidence-url-atom))}
-                       "Add"]]
+                                             (reset! resource-input-mode nil))}
+                       (t :core/Add)]]
           nil)]])))
 
 
@@ -237,10 +238,7 @@
   (let [settings-tab (first (plugin-fun (session/get :plugins) "settings" "settings_tab_content"))
         evidence-name-atom (cursor state [:evidence :name])
         evidence-description-atom (cursor state [:evidence :description])
-        evidence-audience-atom (cursor state [:evidence :audience])
-        evidence-genre-atom (cursor state [:evidence :genre])
         evidence-url-atom (cursor state [:evidence :url])
-        evidence-narrative-atom (cursor state [:evidence :narrative])
         page-input-enabled? (cursor state [:page_input])
         message (cursor state [:evidence :message])
         show-url? (cursor state [:evidence :show_url])
@@ -257,9 +255,8 @@
       [:div
        [:div.form-horizontal
         [:div.form-group
-         [:label.col-md-3 "Add evidence page"]
+         [:label.col-md-9.sub-heading "Add evidence resource"]
          [:div.col-md-9 {:style {:margin-top "10px" :margin-bottom "10px"}}
-          ;[:div {:style {:float "left" :margin-right "10px"}} [:i.fa.fa-plus]]
           [:div {:style {:float "left"}}
            [:div {:style {:float "left" :margin-right "10px"}}[:a {:style {:margin "10px"} :href "#"  :on-click #(toggle-input-mode :url_input state)} [:i.fa.fa-link] (t :admin/Url)]]
            [:div {:style {:float "left" :margin-right "10px"}} [:a {:href "#" :on-click #(do (toggle-input-mode :page_input state)
@@ -269,47 +266,34 @@
                                                :on-click #(do
                                                             (.preventDefault %)
                                                             (toggle-input-mode :file_input state))
-                                               } [:i.fa.fa-file] (t :page/Files)]]]]
+                                               } [:i.fa.fa-file] (t :page/Files)]]]]]
 
          [selected-url state init-data]
-         [resource-input state]]
+         [resource-input state]
 
         (if @show-form
           [:div
            [:div.form-group
             [:label.col-md-3 "Name"]
             [:div.col-md-9
-             [input {:name "name" :atom evidence-name-atom :placeholder "input a name" :type "text"} nil]]]
-           [:div.form-group
-            [:label.col-md-3 "Audience"]
-            [:div.col-md-9
-             [input {:name "audience" :atom evidence-audience-atom :placeholder "" :type "text"} nil]]]
-           [:div.form-group
-            [:label.col-md-3 "Genre"]
-            [:div.col-md-9
-             [input {:name "genre" :atom evidence-genre-atom :placeholder "" :type "text"} nil]]]
-
+             [input {:name "name" :atom evidence-name-atom :type "text"} nil]]]
            [:div.form-group
             [:label.col-md-3 (t :page/Description)]
             [:div.col-md-9
-             [input {:name "Description" :rows 5 :cols 40 :atom evidence-description-atom } true]]]
+             [input {:name "Description" :rows 5 :cols 40 :atom evidence-description-atom } true]]]]
 
-           [:div.form-group
-            [:label.col-md-3 (t :badge/Narrative)]
-            [:div.col-md-9
-             [input {:name "Narrative" :rows 5 :cols 40 :atom evidence-narrative-atom } true]]]
-           ]
-          [:div
+          [:div [:i.fa.fa-plus]
            [:a {:href "#" :on-click #(do (.preventDefault %) (reset! show-form true))} "Add more info about this evidence"]])
-                   [:div
-            [:button {:type         "button"
-                      :class        "btn btn-primary"
-                      :on-click     #(save-badge-evidence data state init-data)}
-             (t :badge/Save)]
-            [:button {:type "button"
-                      :class "btn btn-primary"
-                      :on-click #(do (.preventDefault %) (swap! state assoc :tab [settings-tab (dissoc data :evidence) state init-data] :tab-no 2))}
-             (t :core/Cancel)]]]]]]))
+         [:hr]]
+        [:div
+         [:button {:type         "button"
+                   :class        "btn btn-primary"
+                   :on-click     #(save-badge-evidence data state init-data)}
+          (t :badge/Save)]
+         [:button {:type "button"
+                   :class "btn btn-primary"
+                   :on-click #(do (.preventDefault %) (swap! state assoc :tab [settings-tab (dissoc data :evidence) state init-data] :tab-no 2))}
+          (t :core/Cancel)]]]]]))
 
 (defn delete-evidence! [id data state init-data]
   (let [init-settings (first (plugin-fun (session/get :plugins) "modal" "show_settings_dialog"))]
@@ -317,41 +301,13 @@
       (path-for (str "/obpv1/badge/evidence/" id))
       { :params {:user_badge_id (:id data)}
         :handler (fn [r]
-                   (if (= "success" (:status r))
-                     (init-settings (:id data) state init-data "settings")
-                     ;(swap! state assoc :tab [settings-tab data state init-data] :tab-no 2)
-                     )
-                   )}
-      )
-    ))
-
-#_(defn evidence-list [data state init-data]
-    (reduce (fn [r evidence]
-              (let [{:keys [genre description narrative name audience id url mtime ctime]} evidence]
-                (conj r
-                      [:div.col-md-9.evidence-block
-                       [:a {:href "#" :on-click #(do (.preventDefault %)
-                                                   (init-evidence-form evidence state true)
-                                                   (swap! state assoc :tab [evidence-form data #_(assoc data :evidence evidence) state init-data]))}
-                        [:div [:div {:style {:float "left"}} [:i.fa.fa-link]]  url]
-                        (if name [:div.inline [:label (t :badge/Name) ": "] name])
-                        (if narrative [:div [:label (t :badge/Narrative) ": "] narrative])
-                        (if description [:div [:label (t :admin/Description) ": "]  description])
-                        (if genre [:div [:label (t :badge/Genre) ": "] genre])
-                        (if audience [:div [:label (t :badge/Audience) ": "] audience])
-                        ]
-                       [:button {:type "button"
-                                 :aria-label "OK"
-                                 :class "close"
-                                 :on-click #(delete-evidence! id data state init-data)}
-                        [:span {:aria-hidden "true"
-                                :dangerouslySetInnerHTML {:__html "&times;"}}]]
-                       ])
-                )) [:div.evidence-list] (:evidences data)))
+                   (when (= "success" (:status r))
+                     (init-settings (:id data) state init-data "settings")))})))
 
 (defn evidence-list [data state init-data]
   (reduce (fn [r evidence]
-            (let [{:keys [genre description narrative name audience id url mtime ctime]} evidence]
+            (let [{:keys [genre description narrative name audience id url mtime ctime]} evidence
+                  text ()]
               (conj r
                     [:div.panel.panel-default
                      [:div.panel-heading {:id (str "heading" id)
@@ -359,36 +315,31 @@
                       [:div.panel-title
                        [:a {:href "#" :on-click #(do (.preventDefault %)
                                                    (init-evidence-form evidence state true)
-                                                   (swap! state assoc :tab [evidence-form data state init-data] :show_form true))}
-                        [:div [:div {:style {:float "left"}} [:i.fa.fa-link]]  url]]
-                       (if (or genre description narrative name audience)
+                                                   (swap! state assoc :tab [evidence-form data state init-data] :input_mode nil))}
+                        [:div.url [:div {:style {:float "left"}} [:i.fa.fa-link]]  url]]
+                       (when-not (every? #(blank? %) (vector genre description narrative name audience))
                          [:div.show-more [:a {:role "button" :data-toggle "collapse"
-                          :data-parent "#accordion"
-                          :href (str "#collapse" id)
-                              :aria-expanded "true"
-                              :aria-controls (str "collapse" id)} "show more"]]
-                         )
-                       ]
-                                             [:div [:button {:type "button"
-                                 :aria-label "OK"
-                                 :class "close panel-close"
-                                 :on-click #(delete-evidence! id data state init-data)}
-                        [:span {:aria-hidden "true"
-                                :dangerouslySetInnerHTML {:__html "&times;"}}]]]
+                                              :data-parent "#accordion"
+                                              :href (str "#collapse" id)
+                                              :aria-expanded "true"
+                                              :aria-controls (str "collapse" id)} (t :admin/Showmore)]])]
+                      [:div [:button {:type "button"
+                                      :aria-label "OK"
+                                      :class "close panel-close"
+                                      :on-click #(delete-evidence! id data state init-data)}
+                             [:span {:aria-hidden "true"
+                                     :dangerouslySetInnerHTML {:__html "&times;"}}]]]
                       ]
-                     (if (or genre description narrative name audience)
-                     [:div {:id (str "collapse" id)
-                            :class "panel-collapse collapse"
-                            :role "tabpanel"
-                            :aria-labelledby (str "heading" id)}
-                      [:div.panel-body
-                       (if name [:div.inline [:label (t :badge/Name) ": "] name])
-                        (if narrative [:div [:label (t :badge/Narrative) ": "] [:div narrative]])
-                        (if description [:div [:label (t :admin/Description) ": "]  [:div description]])
-                        (if genre [:div [:label (t :badge/Genre) ": "] genre])
-                        (if audience [:div [:label (t :badge/Audience) ": "] audience])
-                       ]])])
-              )
+                     (when-not (every? #(blank? %) (vector genre description narrative name audience))
+                       [:div {:id (str "collapse" id)
+                              :class "panel-collapse collapse"
+                              :role "tabpanel"
+                              :aria-labelledby (str "heading" id)}
+                        [:div.panel-body.evidence-panel-body {:style {:padding-top "10px"}}
+                         (when-not (blank? name) [:div.inline [:label (t :badge/Name) ": "] name])
+                         (when-not (blank? description) [:div [:label (t :admin/Description)]  [:div.description description]])
+                         ]])])
+  )
             )[:div {:id "accordion" :class "panel-group evidence-list" :role "tablist" :aria-multiselectable "true"}] (:evidences data)))
 
 (defn evidence-block [data state init-data]
@@ -400,8 +351,11 @@
     [:div.col-md-9 {:class "new-evidence"}
      [:i.fa.fa-plus-square.fa-sm]
      [:a {:href "#" :on-click #(do (.preventDefault %)
-                                 (swap! state assoc :evidence nil)
+                                 (swap! state assoc :evidence nil
+                                        :show_form nil
+                                        :input_mode nil)
                                  (swap! state assoc :tab [evidence-form data state init-data] :tab-no 2))} (t :badge/Addnewevidence)]]]
+
 
    [:div.form-group
     [:div.col-md-9 [evidence-list data state init-data]]]
