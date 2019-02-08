6c6bcdaaa059b4bcbcfb49ee52473f58643658ee
isaakay111 <ogunlolui@gmail.com>
2019-02-08 10:21:13 +0200

    user badge endorsement tables

--- resources/migrations/badge/sql/201902071407-user-badge-endorsement.up.sql
+++ resources/migrations/badge/sql/201902071407-user-badge-endorsement.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user_badge_endorsement (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_badge_id` bigint(20) UNSIGNED NOT NULL
+  `user_badge_id` bigint(20) UNSIGNED NOT NULL,
   `endorser_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `endorser_firstname` VARCHAR(255) NOT NULL,
-  `endorser_lastname` VARCHAR(255) NOT NULL,
-  `endorser_email` VARCHAR(255) NOT NULL,
   `content` text,
-  `ctime` bigint UNSIGNED DEFAULT NULL
+  `status` enum('pending','accepted','declined') DEFAULT 'pending',
+  `ctime` bigint UNSIGNED DEFAULT NULL,
+  `mtime` bigint UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
