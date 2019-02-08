f6b4cfde2f4edf50788379870cb888574a95bb4a
isaakay111 <ogunlolui@gmail.com>
2019-02-08 10:24:07 +0200

    add user badge endorsement feature

---
+++ src/clj/salava/badge/endorsement.clj
@@ -0,0 +1,81 @@
+(ns salava.badge.endorsement
+  (:require [yesql.core :refer [defqueries]]
+            [salava.core.util :refer [get-db]]
+            [slingshot.slingshot :refer :all]
+            [clojure.tools.logging :as log]))
+
+(defqueries "sql/badge/main.sql")
+
+(defn badge-owner? [ctx badge-id user-id]
+  (let [owner (select-badge-owner {:id badge-id} (into {:result-set-fn first :row-fn :user_id} (get-db ctx)))]
+    (= owner user-id)))
+
+(defn endorsement-owner? [ctx endorsement-id user-id]
+  (let [owner (select-endorsement-owner {:id endorsement-id} (into {:result-set-fn first :row-fn :endorser_id} (get-db ctx)))]
+    (= owner user-id)))
+
+(defn request-endorsement [])
+
+(defn endorse! [ctx user-badge-id user-id content]
+  (try+
+    (if (badge-owner? ctx user-badge-id user-id)
+      (throw+ {:status "error" :message "User cannot endorse himself"})
+      (when-let [id (->> (insert-user-badge-endorsement<! {:user_badge_id user-badge-id
+                                                           :endorser_id user-id
+                                                           :content content} (get-db ctx))
+                         :generated_key)]
+        {:id id :status "success"}))
+    (catch Object _
+      (log/error _)
+      {:status "error"})))
+
+(defn update-status! [ctx user-id user-badge-id endorsement-id status]
+  (try+
+    (when (badge-owner? ctx user-badge-id user-id)
+      (update-endorsement-status! {:id endorsement-id :status status} (get-db ctx))
+      {:status "success"})
+    (catch Object _
+      (log/error _)
+      {:status "error"})))
+
+(defn delete! [ctx user-badge-id endorsement-id user-id]
+  (try+
+    (if (or (endorsement-owner? ctx endorsement-id user-id) (badge-owner? ctx user-badge-id user-id ))
+      (do (delete-user-badge-endorsement! {:id endorsement-id} (get-db ctx))
+      {:status "success"})
+      {:status "error"}
+      )
+    (catch Object _
+      (log/error _)
+      {:status "error"})))
+
+#_(defn edit!
+  "Edit endorsement.
+  Does this reset status and promp user to receive again"
+  [ctx endorsement-id user-id])
+
+(defn badge-endorsements [ctx user-badge-id]
+  (get-user-badge-endorsements {:user_badge_id user-badge-id} (get-db ctx)))
+
+
+
+(defn recieved-endorsements [ctx user-id])
+(defn all-given-endorsements [ctx endorser-id])
+
+
+;;Endorse a badge earner
+;Accept or decline endorsement
+;Send information to obf
+;Pick from suggested phrases or write own endorsement
+;Endorsements show in user profile
+;Notifications for new endorsements in stream where user can accept or reject.
+;User cannot endorse themselves
+;Delete endorsement?
+;GDPR
+;badge-pdf
+;see declined-endorsements
+;request-endorsement
+;;edit-endorsement
+;;only person who has given the endorsement can delete it
+;;should the endorsement be editable, if yes does the user have to accept or decline after every edit
+;;update endorsement
