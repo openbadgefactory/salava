6816f19e497ed4637420c11c4e0126dbcf5b5719
isaakay111 <ogunlolui@gmail.com>
2019-03-14 11:14:20 +0200

    add more events to notifications block

--- src/cljs/salava/social/ui/dashboard.cljs
+++ src/cljs/salava/social/ui/dashboard.cljs
@@ -57,221 +57,259 @@
         [:p.content-heading (:message message)]
         [:span.date (date-from-unix-time (* 1000 ctime) "days") ]]]]]))
 
+(defn publish-event-badge [event state]
+  (let [{:keys [subject verb image_file message ctime event_id name object first_name last_name]}  event]
+    [:div {:class "media"}
+     ;(hide-event event_id state)
+     [:a {:href "#"
+          :on-click #(do
+                       (mo/open-modal [:badge :info] {:badge-id object})
 
-(defn welcome-block [state]
-  [:div#welcome-block {:class "block"}
-   [:div.welcome-block.block-content.row
-    [:a {:data-toggle "collapse" :href "#hidden" :role "button" :aria-expanded "false" :aria-controls "hidden"}
-   [:div.content
-    [:div
-                                                                                                                [:i.fa.fa-chevron-down.icon]
-                                                                                                                "Welcome back to open badge passport"]]
-    [:div.collapse.hidden-content {:id "hidden" }
-     [:p "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
-     ]
-    ]]])
+                       (.preventDefault %) )}
+      [:div.media-left
+       [:img {:src (str "/" image_file)} ]]
+      [:div.media-body
+       [:div.content-text
+        [:p.content-heading (str (t :social/User) " " first_name " " last_name " " (t :social/Publishedbadge) " " name)]
+        [:span.date (date-from-unix-time (* 1000 ctime) "days")]
+        ]
+       ;[:div.date (date-from-unix-time (* 1000 ctime) "days") ]
+       #_[:div [:h3 {:class "media-heading"}
+              [:a {:href "#"
+                   :on-click #(do
+                                (mo/open-modal [:badge :info] {:badge-id object})
+                                (.preventDefault %) )} (str (t :social/User) " " first_name " " last_name " " (t :social/Publishedbadge) " " name)]]
+        [:div.media-body
+         (t :social/Publishedbadgetext)]]
+       ]]])
+     )
 
-(defn notifications-block [state]
-  (let [events (:events @state)]
-    [:div {:class "box col-md-4" }
-     [:div#box_1
-      [:div.col-md-12.block
-       ;(prn @state)
-       [:div.notifications-block.row_1;.block-content
-        [:div.heading_1 [:i.fa.fa-rss.icon]
-         [:a {:href "social/stream"} [:span.title "notifications"]] [:span.badge (:events_count @state)]]
-        (reduce (fn [r event]
-                  (conj r [:div.notification-div.ax_default
-                           (cond
-                             (and (= "badge" (:type event)) (= "follow" (:verb event))) (follow-event-badge event state)
-                             (and (= "user" (:type event)) (= "follow" (:verb event))) (stream/follow-event-user event state)
-                             (and (= "badge" (:type event)) (= "publish" (:verb event))) (stream/publish-event-badge event state)
-                             (and (= "page" (:type event)) (= "publish" (:verb event))) (stream/publish-event-page event state)
-                             (= "advert" (:type event)) (stream/badge-advert-event event state)
-                             (= "message" (:verb event)) [message-event event state]
-                             :else "")
-                           ])
-                  )[:div.content] (->> events (remove #(= (:verb %) "ticket"))))
-        ]]]]))
 
-(defn badges-block [state]
-  (fn []
-    (let [badges (:badges @state )]
-      [:div {:class "box col-md-5 "}
-       [:div#box_2
-        [:div.col-md-12.block
-         [:div.badge-block;.block-content
-          [:div.heading_1
-           [:i.fa.fa-certificate.icon]
-           [:a {:href (path-for "/badge")} [:span.title "Badges"]]]
-          [:button.btn.button "Earn badges"]
+     (defn welcome-block [state]
+       [:div#welcome-block {:class "block"}
+        [:div.welcome-block.block-content.row
+         [:a {:data-toggle "collapse" :href "#hidden" :role "button" :aria-expanded "false" :aria-controls "hidden"}
           [:div.content
-           [:div.stats
-            [:div.total-badges
-             [:p.num (get-in @state [:stats :badge_count] 0)]
-             [:p.desc (t :badge/Badges)]]
-            [:div
-             [:p.num (->> (get-in @state [:stats :badge_views])
-                          (reduce #(+ %1 (:reg_count %2) (:anon_count %2)) 0))]
-             [:p.desc (t :badge/Badgeviews)]]]
+           [:div
+            [:i.fa.fa-chevron-down.icon]
+            "Welcome back to open badge passport"]]
+          [:div.collapse.hidden-content {:id "hidden" }
+           [:p "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+           ]
+          ]]])
 
-           (when (seq (:pending-badges @state)) [:div.pending
-                                                 [:p.header (t :badge/Pendingbadges)]
-                                                 (if (seq (:pending-badges @state))
-                                                   (reduce (fn [r badge]
-                                                             (conj r [:a {:href (path-for "/badge")} [:img {:src (str "/" (:image_file badge)) :alt (:name badge) :title (:name badge)}]])
-                                                             ) [:div] (take 5 (:pending-badges @state)))
-                                                   )])
-           [:div.badges
-            [:p.header (t :badge/Lastestearnedbadges)]
-            (reduce (fn [r badge]
-                      (conj r [:a {:href "#" :on-click #(do
-                                                          (.preventDefault %)
-                                                          (mo/open-modal [:badge :info] {:badge-id (:id badge )}))} [:img {:src (str "/" (:image_file badge)) :alt (:name badge) :title (:name badge)}]])
-                      ) [:div] badges)
+     (defn notifications-block [state]
+       (let [events (->> (:events @state) (take 5))]
+         [:div {:class "box col-md-4" }
+          [:div#box_1
+           [:div.col-md-12.block
+            ;(prn @state)
+            [:div.notifications-block.row_1;.block-content
+             [:div.heading_1 [:i.fa.fa-rss.icon]
+              [:a {:href "social/stream"} [:span.title "notifications"]] [:span.badge (:events_count @state)]]
+             (reduce (fn [r event]
+                       (conj r [:div.notification-div.ax_default
+                                (cond
+                                  (and (= "badge" (:type event)) (= "follow" (:verb event))) (follow-event-badge event state)
+                                  (and (= "user" (:type event)) (= "follow" (:verb event))) (stream/follow-event-user event state)
+                                  (and (= "badge" (:type event)) (= "publish" (:verb event))) (stream/publish-event-badge event state)
+                                  (and (= "page" (:type event)) (= "publish" (:verb event))) (stream/publish-event-page event state)
+                                  (= "advert" (:type event)) (stream/badge-advert-event event state)
+                                  (= "message" (:verb event)) [message-event event state]
+                                  :else "")
+                                ])
+                       )[:div.content] (->> events (remove #(= (:verb %) "ticket"))))
+             ]]]]))
+
+     (defn latest-earnable-badges []
+       (let [block (first (plugin-fun (session/get :plugins)  "application" "latestearnablebadges"))]
+         (if block [block] [:div ""])))
+
+     (defn application-button []
+       (let [button (first (plugin-fun (session/get :plugins) "application" "button"))]
+         (if button
+           [button]
+           [:div ""])))
+
+     (defn badges-block [state]
+       (fn []
+         (let [badges (:badges @state )]
+           [:div {:class "box col-md-5 "}
+            [:div#box_2
+             [:div.col-md-12.block
+              [:div.badge-block;.block-content
+               [:div.heading_1
+                [:i.fa.fa-certificate.icon]
+                [:a {:href (path-for "/badge")} [:span.title "Badges"]]]
+               [application-button]; [:button.btn.button "Earn badges"]
+               [:div.content
+                [:div.stats
+                 [:div.total-badges
+                  [:p.num (get-in @state [:stats :badge_count] 0)]
+                  [:p.desc (t :badge/Badges)]]
+                 [:div
+                  [:p.num (->> (get-in @state [:stats :badge_views])
+                               (reduce #(+ %1 (:reg_count %2) (:anon_count %2)) 0))]
+                  [:p.desc (t :badge/Badgeviews)]]]
+
+                (when (seq (:pending-badges @state)) [:div.pending
+                                                      [:p.header (t :badge/Pendingbadges)]
+                                                      (if (seq (:pending-badges @state))
+                                                        (reduce (fn [r badge]
+                                                                  (conj r [:a {:href (path-for "/badge")} [:img {:src (str "/" (:image_file badge)) :alt (:name badge) :title (:name badge)}]])
+                                                                  ) [:div] (take 5 (:pending-badges @state)))
+                                                        )])
+                [:div.badges
+                 [:p.header (t :badge/Lastestearnedbadges)]
+                 (reduce (fn [r badge]
+                           (conj r [:a {:href "#" :on-click #(do
+                                                               (.preventDefault %)
+                                                               (mo/open-modal [:badge :info] {:badge-id (:id badge )}))} [:img {:src (str "/" (:image_file badge)) :alt (:name badge) :title (:name badge)}]])
+                           ) [:div] badges)
+                 ]
+                [latest-earnable-badges]
+                #_[:div.badges
+                   [:p.header (t :badge/Latestearnablebadges)]
+                   ;[earnable-badges state]
+                   ]
+                ]
+               ]]]])))
+
+     (defn explore-block [state]
+       [:div.box.col-md-4
+
+        [:div#box_1 {:class "row_2"}
+         [:div.col-md-12.block
+          [:div.row_2;.block-content
+           [:div.heading_1
+            [:i.fa.fa-search.icon]
+            [:span.title "Explore"]]
+           [:div.content
+            [:div.info-block.badge-connection
+             [:a {:href (path-for "/gallery/badges")}
+              [:div.info
+               [:i.fa.fa-certificate.icon]
+               [:div.text
+                [:p.num (get-in @state [:gallery :badges])]
+                [:p.desc (t :badge/Badges)]]]]]
+            [:div.info-block.page
+             [:a {:href (path-for "/gallery/pages")}[:div.info
+                                                     [:i.fa.fa-file.icon]
+                                                     [:div.text
+                                                      [:p.num (get-in @state [:gallery :pages])]
+                                                      [:p.desc (t :page/Pages)]]]]]
+            [:div.info-block
+             [:a {:href (path-for "/gallery/profiles")}[:div.info
+                                                        [:i.fa.fa-user.icon]
+                                                        [:div.text
+                                                         [:p.num (get-in @state [:gallery :profiles :all])]
+                                                         [:p.desc (t :gallery/Profiles)]]]]]
+
             ]
-           [:div.badges
-            [:p.header (t :badge/Latestearnablebadges)]
-            ;[earnable-badges state]
+           ]]]])
+
+     (defn user-connections-stats []
+       (let [blocks (first (plugin-fun (session/get :plugins) "block" "userconnectionstats"))]
+         (if blocks
+           [blocks]
+           [:div ""])))
+
+     (defn connections-block [state]
+       [:div.box.col-md-5
+        [:div#box_2
+         [:div.col-md-12.block
+          [:div.row_2
+           [:div
+            [:div.heading_1 [:i.fa.fa-group.icon]
+             [:span.title
+              "Connections"]]
+            [:div.content;.connections-block;.block-content
+             [user-connections-stats]
+             [:div.info-block.badge-connection
+              [:a {:href (path-for "/connections/badge")}
+               [:div.info
+                [:i.fa.fa-certificate.icon]
+                [:div.text
+                 [:p.num (get-in @state [:connections :badges])]
+                 [:p.desc (t :badge/Badges)]]]]]
+             [:div.info-block.endorsement
+              [:a {:href (path-for "/connections/endorsement")}
+               [:div.info
+                [:i.fa.fa-thumbs-up.icon]
+                [:div.text
+                 [:p.num (:endorsing @state)]
+                 [:p.desc "Endorsing" #_(t :badge/Endorsing)]
+                 ]
+                ]
+               ]]
+             [:div.info-block.endorsement
+              [:a {:href (path-for "/connections/endorsement")}
+               [:div.info
+                (when (pos? (:pending-endorsements @state)) [:span.badge (:pending-endorsements @state)])
+                [:i.fa.fa-thumbs-up.icon]
+
+                [:div.text
+                 [:p.num (:endorsers @state)]
+                 [:p.desc "Endorsers" #_(t :badge/Endorsing)]
+                 ]
+                ]
+               ]]
+             ]]]]]])
+
+     (defn profile-block [state]
+       [:div.box.col-md-3
+        [:div.row_1 {:id "box_1"}
+         [:div.col-md-12.block
+          [:div.profile-block;.block-content
+           [:div.heading_1
+            [:i.fa.fa-user.icon]
+            [:span.title
+             "Profile"]]]]]])
+
+     (defn help-block [state]
+       [:div {:class "box col-md-3"}
+        [:div#box_3
+         [:div.col-md-12.block
+          [:div.row_2.help;.getting-started.block-content
+           [:div.heading_1
+            [:i.fa.fa-info-circle.icon]
+            [:span.title.help
+             "Help"]]
+           [:div.content
+            [:a {:href (str (path-for "/user/profile/") (session/get-in [:user :id]))}[:p (t :badge/Iwanttoseeprofile)]]
+            [:a {:href (str (path-for "/user/edit/profile"))}[:p (t :badge/Iwanttoeditprofile)]]
+            [:a {:href (str (path-for "/badge"))}[:p (t :badge/Iwanttoseemysbadges)]]
+            (when (some #(= % :extra/application) (session/get :plugins))[:p (t :badge/Iwanttoearnnewbadges)])
+            [:a {:href (str (path-for "/gallery/badges"))} [:p (t :badge/Iwanttofindbadges)]]
+
             ]
            ]
-          ]]]])))
+          ]]]
+       )
 
-(defn explore-block [state]
-  [:div.box.col-md-4
+     (defn content [state]
+       [:div#dashboard-container
+        [m/modal-window]
+        [welcome-block state]
+        [:div.row
+         [notifications-block state]
+         [badges-block state]
+         [profile-block state]
+         ]
 
-   [:div#box_1 {:class "row_2"}
-    [:div.col-md-12.block
-     [:div.row_2;.block-content
-      [:div.heading_1
-       [:i.fa.fa-search.icon]
-       [:span.title "Explore"]]
-      [:div.content
-       [:div.info-block.badge-connection
-        [:a {:href (path-for "/gallery/badges")}
-         [:div.info
-         [:i.fa.fa-certificate.icon]
-         [:div.text
-          [:p.num (get-in @state [:gallery :badges])]
-          [:p.desc (t :badge/Badges)]]]]]
-              [:div.info-block.page
-        [:a {:href (path-for "/gallery/pages")}[:div.info
-         [:i.fa.fa-file.icon]
-         [:div.text
-          [:p.num (get-in @state [:gallery :pages])]
-          [:p.desc (t :page/Pages)]]]]]
-              [:div.info-block
-        [:a {:href (path-for "/gallery/profiles")}[:div.info
-         [:i.fa.fa-user.icon]
-         [:div.text
-          [:p.num (get-in @state [:gallery :profiles :all])]
-          [:p.desc (t :gallery/Profiles)]]]]]
+        [:div.row
+         ;[profile-block state]
 
-       ]
-      ]]]])
-
-(defn user-connections-stats []
-  (let [blocks (first (plugin-fun (session/get :plugins) "block" "userconnectionstats"))]
-    (if blocks
-      [blocks]
-      [:div ""])))
-
-(defn connections-block [state]
-  [:div.box.col-md-5
-   [:div#box_2
-    [:div.col-md-12.block
-     [:div.row_2
-      [:div
-       [:div.heading_1 [:i.fa.fa-group.icon]
-        [:span.title
-         "Connections"]]
-       [:div.content;.connections-block;.block-content
-        [user-connections-stats]
-        [:div.info-block.badge-connection
-         [:a {:href (path-for "/connections/badge")}
-         [:div.info
-          [:i.fa.fa-certificate.icon]
-          [:div.text
-           [:p.num (get-in @state [:connections :badges])]
-           [:p.desc (t :badge/Badges)]]]]]
-        [:div.info-block.endorsement
-         [:a {:href (path-for "/connections/endorsement")}
-         [:div.info
-          [:i.fa.fa-thumbs-up.icon]
-          [:div.text
-           [:p.num (:endorsing @state)]
-           [:p.desc "Endorsing" #_(t :badge/Endorsing)]
-           ]
-          ]
-         ]]
-        [:div.info-block.endorsement
-         [:a {:href (path-for "/connections/endorsement")}
-         [:div.info
-          (when (pos? (:pending-endorsements @state)) [:span.badge (:pending-endorsements @state)])
-          [:i.fa.fa-thumbs-up.icon]
-
-          [:div.text
-           [:p.num (:endorsers @state)]
-           [:p.desc "Endorsers" #_(t :badge/Endorsing)]
-           ]
-          ]
-         ]]
-        ]]]]]])
-
-(defn profile-block [state]
-  [:div.box.col-md-3
-   [:div.row_1 {:id "box_1"}
-    [:div.col-md-12.block
-     [:div.profile-block;.block-content
-      [:div.heading_1
-       [:i.fa.fa-user.icon]
-       [:span.title
-        "Profile"]]]]]])
-
-(defn help-block [state]
-  [:div {:class "box col-md-3"}
-   [:div#box_3
-    [:div.col-md-12.block
-     [:div.row_2.help;.getting-started.block-content
-      [:div.heading_1
-       [:i.fa.fa-info-circle.icon]
-       [:span.title.help
-        "Help"]]
-      [:div.content
-       [:a {:href (str (path-for "/user/profile/") (session/get-in [:user :id]))}[:p (t :badge/Iwanttoseeprofile)]]
-       [:a {:href (str (path-for "/user/edit/profile"))}[:p (t :badge/Iwanttoeditprofile)]]
-       [:a {:href (str (path-for "/badge"))}[:p (t :badge/Iwanttoseemysbadges)]]
-       (when (some #(= % :extra/application) (session/get :plugins))[:p (t :badge/Iwanttoearnnewbadges)])
-       [:a {:href (str (path-for "/gallery/badges"))} [:p (t :badge/Iwanttofindbadges)]]
-
-       ]
-      ]
-     ]]]
-  )
-
-(defn content [state]
-  [:div#dashboard-container
-   [m/modal-window]
-   [welcome-block state]
-   [:div.row
-    [notifications-block state]
-    [badges-block state]
-    [profile-block state]
-    ]
-
-   [:div.row
-    ;[profile-block state]
-
-    [explore-block state]
-    [connections-block state]
-    [help-block]
+         [explore-block state]
+         [connections-block state]
+         [help-block]
 
 
-    ]])
+         ]])
 
 
-(defn handler [site-navi]
-  (let [state (atom {})]
-    (init-dashboard state)
-    (fn [] (layout/dashboard site-navi [content state]))))
+     (defn handler [site-navi]
+       (let [state (atom {})]
+         (init-dashboard state)
+         (fn [] (layout/dashboard site-navi [content state]))))
 
