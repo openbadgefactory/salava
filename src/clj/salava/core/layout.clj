9966d01fd0ef92521a9f01cf73f37ce1cebaea04
isaakay111 <ogunlolui@gmail.com>
2019-03-19 11:13:50 +0200

    add last login info to session

--- src/clj/salava/core/layout.clj
+++ src/clj/salava/core/layout.clj
@@ -134,7 +134,8 @@
 (defn main-response [ctx current-user flash-message meta-tags]
   (let [user (if current-user (-> (u/user-information ctx (:id current-user))
                                   (assoc :terms (:status (u/get-accepted-terms-by-id ctx (:id current-user))))
-                                  (assoc  :real-id (:real-id current-user))))] ;;real-id is for admin login as user
+                                  (assoc  :real-id (:real-id current-user) ;;real-id is for admin login as user
+                                          :last-visited (u/last-visited ctx (:id current-user)))))] ;;user's previous visit
     (-> (main-view (assoc ctx :user user :flash-message flash-message) meta-tags)
         (ok)
         (content-type "text/html; charset=\"UTF-8\""))))
