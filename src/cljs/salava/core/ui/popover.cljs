76ce4fc5d3358d88e204484d1a8142b3df2c1ce0
isaakay111 <ogunlolui@gmail.com>
2019-03-22 16:29:46 +0200

    enable styles in popover

--- src/cljs/salava/core/ui/popover.cljs
+++ src/cljs/salava/core/ui/popover.cljs
@@ -3,13 +3,15 @@
             [salava.core.i18n :refer [t]]))
 
 ;;pop-up placement options ["top" "bottom" "right" "left"]
-(defn info [content placement]
-  (create-class {:reagent-render (fn []
-                                   [:a.popup-info {:tabIndex "0"
-                                        :data-toggle "popover"
-                                        :data-trigger "focus"
-                                        :data-content content
-                                        :data-placement placement
-                                        :href "#"} [:i.fa.fa-question-circle.fa-sm]])
-                 :component-did-mount (fn []
-                                        (.getScript (js* "$") "/js/pop-over.js"))}))
+(defn info [opts]
+  (let [{:keys [content placement style]} opts]
+    (create-class {:reagent-render (fn []
+                                     [:a.popup-info {:tabIndex "0"
+                                                     :data-toggle "popover"
+                                                     :data-trigger "focus"
+                                                     :data-content content
+                                                     :data-placement placement
+                                                     :href "#"
+                                                     :style style} [:i.fa.fa-question-circle.fa-sm]])
+                   :component-did-mount (fn []
+                                          (.getScript (js* "$") "/js/pop-over.js"))})))
