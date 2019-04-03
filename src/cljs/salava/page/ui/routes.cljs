cba29f104ab2ce8fbff6b6f85613133b66fb36ab
isaakay111 <ogunlolui@gmail.com>
2019-04-03 14:51:11 +0300

    remove page from top navigation

--- src/cljs/salava/page/ui/routes.cljs
+++ src/cljs/salava/page/ui/routes.cljs
@@ -1,4 +1,4 @@
-(ns salava.page.ui.routes 
+(ns salava.page.ui.routes
   (:require [salava.core.ui.layout :as layout]
             [salava.core.i18n :as i18n :refer [t]]
             [salava.core.ui.helper :refer [base-path]]
@@ -22,8 +22,8 @@
                                      [["/preview/" :page-id] preview/handler]]})
 
 (defn ^:export navi [context]
-  {(str (base-path context) "/page")                 {:weight 30 :title (t :page/Pages) :top-navi true :breadcrumb (t :page/Pages " / " :page/Mypages)}
-   (str (base-path context) "/page/mypages")         {:weight 31 :title (t :page/Mypages) :site-navi true :breadcrumb (t :page/Pages " / " :page/Mypages)}
+  {;(str (base-path context) "/page")                 #_{:weight 31 :title (t :page/Pages) :top-navi true :breadcrumb (t :page/Pages " / " :page/Mypages)}
+   (str (base-path context) "/page/mypages")         {:weight 32 :title (t :page/Mypages) :site-navi true :breadcrumb (t :page/Pages " / " :page/Mypages)}
    (str (base-path context) "/page/view/\\d+")       {:breadcrumb (t :page/Pages " / " :page/Viewpage)}
    (str (base-path context) "/page/edit/\\d+")       {:breadcrumb (t :page/Pages " / " :page/Editpage)}
    (str (base-path context) "/page/edit_theme/\\d+") {:breadcrumb (t :page/Pages " / " :page/Choosetheme)}
