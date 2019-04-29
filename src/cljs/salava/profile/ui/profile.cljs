71deada37d488e80c0e13acefb22325a76979fd5
isaakay111 <ogunlolui@gmail.com>
2019-04-29 14:33:22 +0300

    new profile initial

---
+++ src/cljs/salava/profile/ui/profile.cljs
@@ -0,0 +1,13 @@
+(ns salava.profile.ui.profile
+  (:require [salava.core.ui.helper :refer []]
+            [salava.core.ui.ajax-utils :as ajax]))
+
+(defn profile-navi [state])
+
+(defn view-profile [state])
+
+(defn edit-profile [state])
+
+(defn init-data [])
+
+(defn handler [])
