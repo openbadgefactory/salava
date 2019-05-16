3e44ac0a9abe65030eb0e1fc6330e98c3410d5e5
isaakay111 <ogunlolui@gmail.com>
2019-05-16 23:00:47 +0300

    refactor quicklinks, add map button to dahsboard initial

--- src/cljs/salava/profile/ui/routes.cljs
+++ src/cljs/salava/profile/ui/routes.cljs
@@ -1,6 +1,6 @@
 (ns salava.profile.ui.routes
   (:require [salava.core.ui.layout]
-            [salava.core.ui.helper :refer [base-path]]
+            [salava.core.ui.helper :refer [base-path path-for]]
             [salava.user.ui.profile :as profile]
             [salava.core.i18n :as i18n :refer [t]]
             [salava.page.ui.my :as page]
@@ -8,7 +8,8 @@
             [salava.profile.ui.profile :as p]
             [salava.profile.ui.embed :as embed]
             [salava.profile.ui.edit :as pe]
-            [salava.profile.ui.modal :as m]))
+            [salava.profile.ui.modal :as m]
+            [reagent.session :as session]))
 
 
 
@@ -20,3 +21,13 @@
 (defn ^:export navi [context]
   {(str (base-path context) "/profile/\\d+")  {:breadcrumb (t :user/User " / " :user/Profile)}
    (str (base-path context) "/profile/" (get-in context [:user :id])) {:weight 30 :title (t :user/Profile) :top-navi true :site-navi true :breadcrumb (str (t :user/Profile) " / " (get-in context [:user :first_name]) " " (get-in context [:user :last_name]))}})
+
+(defn ^:export quicklinks []
+  [{:title [:p (t :social/Iwanttoseeprofile)]
+    :url (str (path-for "/profile/") (session/get-in [:user :id]))
+    :weight 2}
+
+   {:title [:p (t :social/Iwanttoeditprofile)]
+    :url (str (path-for "/profile/") (session/get-in [:user :id]))
+    :weight 3
+    :function (fn [] (session/put! :edit-mode true))}])
