ca9cca7e802e3c010c257101e8e6fe00015bb0b6
isaakay111 <ogunlolui@gmail.com>
2019-05-17 16:12:44 +0300

    map stats for dashboard

--- src/clj/salava/gallery/db.clj
+++ src/clj/salava/gallery/db.clj
@@ -366,6 +366,7 @@
            }
    :badges {:all (gallery-badges-count {} (into {:result-set-fn first :row-fn :badges_count}(get-db ctx)))
             :since-last-visited (gallery-badges-count-since-last-login {:user_id user-id :last_login last-login} (into {:result-set-fn first :row-fn :badges_count} (get-db ctx)))}
+   :map {:all (all-users-on-map-count {} (into {:result-set-fn first :row-fn :users_count} (get-db ctx)))}
    })
 
 (defn public-by-user [ctx kind user-id current-user-id]
