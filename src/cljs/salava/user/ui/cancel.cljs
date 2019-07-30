32838356890e95c8f88af716ffbeef0dbb5d7888
isaakay111 <ogunlolui@gmail.com>
2019-07-30 12:22:43 +0300

    update remove account page

--- src/cljs/salava/user/ui/cancel.cljs
+++ src/cljs/salava/user/ui/cancel.cljs
@@ -23,45 +23,47 @@
 
 (defn cancel-account-modal [state]
   [:div
-       [:div.modal-body
-        [:div.row
-         [:div.col-md-12
-          [:button {:type  "button"
-                    :class  "close"
-                    :data-dismiss "modal"
-                    :aria-label   "OK"
-                    }
-           [:span {:aria-hidden  "true"
-                   :dangerouslySetInnerHTML {:__html "&times;"}}]]]]
-        [:div
-         [:p "All your data will be erased and you will be automatically logged out of the system!"]
-         [:p [:b "Once you remove your account your data cannot be restored!"]]
-         [:form {:class "form-horizontal"}
-          [:div.form-group
-           [:fieldset {:id "export-pdf" :class "col-md-12 checkbox"}
-            [:div.col-md-12 [:label
-                             [:input {:type     "checkbox"
-                                      :on-change (fn [e]
-                                                   (if (.. e -target -checked)
-                                                     (swap! state assoc :confirm-delete "true")(swap! state assoc :confirm-delete "false")
-                                                     ))}]
-                             "Are you sure you want to remove account?"]]]]]]]
-       [:div.modal-footer
-        [:button {:type         "button"
-                  :class        "btn btn-primary"
-                  :data-dismiss "modal"}
-         (t :core/Close)]
-        [:button {:type         "button"
-                  :class        "btn btn-warning"
-                  :disabled (if-not (= "true" (:confirm-delete @state))
-                              "disabled")
-                  :on-click   #(do
-                                 (if (= "true" (:confirm-delete @state))
-                                   (cancel-account state)))
-                  :data-dismiss "modal"
-                  }
-         "Delete"]]]
-  )
+   [:div.modal-body
+    [:div.row
+     [:div.col-md-12
+      [:button {:type  "button"
+                :class  "close"
+                :data-dismiss "modal"
+                :aria-label   "OK"}
+
+       [:span {:aria-hidden  "true"
+               :dangerouslySetInnerHTML {:__html "&times;"}}]]]]
+    [:div
+     [:p (t :user/Confirmaccountremoval1)]
+     [:p [:b (t :user/Confirmaccountremoval2)]]
+     [:form {:class "form-horizontal"}
+      [:div.form-group
+       [:fieldset {:id "export-pdf" :class "col-md-12 checkbox"}
+        [:div.col-md-12 [:label
+                         [:input {:type     "checkbox"
+                                  :on-change (fn [e]
+                                               (if (.. e -target -checked)
+                                                 (swap! state assoc :confirm-delete "true")(swap! state assoc :confirm-delete "false")))}]
+
+                         (t :user/Confirmaccountremoval3)]]]]]]
+    [:div.modal-footer
+     [:button {:type         "button"
+               :class        "btn btn-primary"
+               :style {:margin-bottom "unset"}
+               :data-dismiss "modal"}
+      (t :core/Cancel)]
+     [:button {:type         "button"
+               :class        "btn btn-warning"
+               :style {:margin-bottom "unset"}
+               :disabled (if-not (= "true" (:confirm-delete @state))
+                           "disabled")
+               :on-click   #(do
+                              (if (= "true" (:confirm-delete @state))
+                                (cancel-account state)))
+               :data-dismiss "modal"}
+
+      (t :user/Cancelaccount)]]]])
+
 
 (defn cancel-form [state]
   (let [password-atom (cursor state [:password])]
@@ -91,6 +93,36 @@
                #_(cancel-account state)}
       (t :user/Cancelaccount)]]))
 
+#_(defn content [state]
+   [:div {:id "cancel-account"}
+    [:h1.uppercase-header (t :user/Cancelaccount)]
+    (if (:has-password? @state)
+      [:div
+       [m/modal-window]
+       [:div {:id "cancel-info"}
+        [:p (t :user/Cancelaccountinstructions1)]
+        [:p (t :user/Cancelaccountinstructions2) ":"]
+        [:p (t :user/Cancelaccountinstructions3) ":"]
+        [:ul
+         [:li (t :user/Goto) " " [:a {:href (path-for "/badge/export")} (t :user/Badgeexport)] " " (t :user/page)]
+         [:li {:dangerouslySetInnerHTML
+               {:__html (t :user/Cancelaccountinstructions4)}}]]
+        [:p (t :user/Cancelaccountinstructions5) ":"]
+        [:ul
+         [:li (t :user/Goto) " " [:a {:href (path-for "/badge/export")} (t :user/Badgeexport)] " " (t :user/page)]
+         [:li {:dangerouslySetInnerHTML
+               {:__html (t :user/Cancelaccountinstructions6)}}]
+         [:li {:dangerouslySetInnerHTML
+               {:__html (t :user/Cancelaccountinstructions7)}}]
+         [:li  (t :user/Cancelaccountinstructions8)]]
+        [:p [:b (t :user/Cancelaccountinstructions9)]]]
+       [:div {:class "panel"}
+        [:div.panel-body
+         (cancel-form state)]]]
+      [:div {:class "panel"}
+       [:span (t :oauth/Cannotunlink) " " (t :oauth/Setpasswordfrom)
+                 " " [:a {:href (path-for "/user/edit/password")} (t :user/Passwordsettings)] "."]])])
+
 (defn content [state]
   [:div {:id "cancel-account"}
    [:h1.uppercase-header (t :user/Cancelaccount)]
@@ -99,20 +131,9 @@
       [m/modal-window]
       [:div {:id "cancel-info"}
        [:p (t :user/Cancelaccountinstructions1)]
-       [:p (t :user/Cancelaccountinstructions2) ":"]
-       [:p (t :user/Cancelaccountinstructions3) ":"]
        [:ul
-        [:li (t :user/Goto) " " [:a {:href (path-for "/badge/export")} (t :user/Badgeexport)] " " (t :user/page)]
-        [:li {:dangerouslySetInnerHTML
-              {:__html (t :user/Cancelaccountinstructions4)}}]]
-       [:p (t :user/Cancelaccountinstructions5) ":"]
-       [:ul
-        [:li (t :user/Goto) " " [:a {:href (path-for "/badge/export")} (t :user/Badgeexport)] " " (t :user/page)]
-        [:li {:dangerouslySetInnerHTML
-              {:__html (t :user/Cancelaccountinstructions6)}}]
-        [:li {:dangerouslySetInnerHTML
-              {:__html (t :user/Cancelaccountinstructions7)}}]
-        [:li  (t :user/Cancelaccountinstructions8)]]
+         [:li (t :user/Removeaccountinstruction)]
+         [:li (t :user/Removeaccountinstruction2)]]
        [:p [:b (t :user/Cancelaccountinstructions9)]]]
       [:div {:class "panel"}
        [:div.panel-body
