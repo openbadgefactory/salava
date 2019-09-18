dea7309279106eb34bcd26d5b62731c8494152ec
isaakay111 <ogunlolui@gmail.com>
2019-09-18 12:54:14 +0300

    Add metabadge check to badge cron

--- src/clj/salava/badge/cron.clj
+++ src/clj/salava/badge/cron.clj
@@ -4,9 +4,10 @@
             [clojure.string :as string]
             [clj-time.core :as t]
             [salava.core.util :as u]
-            [salava.core.http :as http]))
+            [salava.core.http :as http]
+            [clojure.data.json :as json]))
 
-(defn- assertion [assertion-url]
+#_(defn- assertion [assertion-url]
   (http/http-req {:socket-timeout 10000
                   :conn-timeout   10000
                   :method :get
@@ -15,10 +16,22 @@
                   :as :json
                   :accept :json}))
 
+(defn- assertion [asr-url]
+ (let [response (->> (http/http-req {:socket-timeout 10000
+                           :conn-timeout 10000
+                           :method :get
+                           :url asr-url
+                           :throw-exceptions false
+                           :accept :json})
+                     (clojure.walk/keywordize-keys))]
+   (if (clojure.string/includes? (get-in response [:headers :Content-Type]) "application/json")
+        (assoc response :body (json/read-str (:body response) :key-fn keyword))
+        nil)))
+
 (defn- revoked? [asr]
   (or (= (:status asr) 410) (get-in asr [:body :revoked])))
 
-(defn- remote-url [factory-url asr]
+#_(defn- remote-url [factory-url asr]
   (when (and (string? factory-url) (map? asr))
     (cond
       (string/starts-with? (get asr :id "") factory-url) factory-url
@@ -27,7 +40,22 @@
            (string/starts-with? (get (http/json-get (get-in asr [:related :id])) :id "") factory-url)) factory-url
       :else nil)))
 
-(defn- issuer-verified? [factory-url asr]
+(defn- remote-url [factory-url asr]
+ (when (and (string? factory-url) (not (clojure.string/blank? factory-url)) (map? asr))
+  (let [related-asr (->> (get asr :related) first :id)]
+   (if (and (not (clojure.string/blank? (:id asr)))
+            (not (clojure.string/blank? related-asr))
+            (= (second (clojure.string/split (get asr :id "") #"/v1")) (second (clojure.string/split related-asr #"/v1")))
+            (= (first (clojure.string/split related-asr #"/v1")) factory-url))
+     (first (clojure.string/split related-asr #"/v1"))
+     (cond
+       (string/starts-with? (get asr :id "") factory-url) factory-url
+       (string/starts-with? (get-in asr [:verify :url] "") factory-url) factory-url
+       (and (= (get-in asr [:related :type]) "Assertion")
+            (string/starts-with? (get (http/json-get (get-in asr [:related :id])) :id "") factory-url)) factory-url
+           :else nil)))))
+
+#_(defn- issuer-verified? [factory-url asr]
   (when (and (string? factory-url) (map? asr) (string/starts-with? (get asr :id "") factory-url))
     (try
       (let [badge (if (map? (:badge asr)) (:badge asr) (http/json-get (:badge asr)))
@@ -38,9 +66,23 @@
           false))
       (catch Throwable _ false))))
 
+(defn- issuer-verified? [factory-url asr]
+ (let [factory-url (remote-url factory-url asr)]
+   (try
+     (let [badge (if (map? (:badge asr)) (:badge asr) (http/json-get (:badge asr)))
+           issuer-url (if (map? (:issuer badge)) (get-in badge [:issuer :id]) (:issuer badge))
+           issuer-id (last (re-find #"/v1/client.*[?&]key=(\w+)" issuer-url))]
+       (if issuer-id
+         (:verified (http/json-get (str factory-url "/v1/client?key=" issuer-id)))
+         false))
+     (catch Throwable _ false))))
+
 (defn- check-badge [ctx db-conn factory-url user-badge]
   (let [asr (assertion (:assertion_url user-badge))
-        delete-user-metabadge (first (u/plugin-fun (u/get-plugins ctx) "db" "clear-user-metabadge!"))]
+        related-asr (->> (get-in asr [:body :related]) first :id)
+        factory-url (remote-url factory-url (:body asr))
+        delete-user-metabadge (first (u/plugin-fun (u/get-plugins ctx) "db" "clear-user-metabadge!"))
+        check-metabadge (first (u/plugin-fun (u/get-plugins ctx) "db" "metabadge?!"))]
     (if (revoked? asr)
       (do
         (jdbc/execute! db-conn
@@ -55,7 +97,8 @@
         (jdbc/execute! t-con ["UPDATE badge SET issuer_verified = ? WHERE id = ? AND remote_url IS NOT NULL"
                               (if (issuer-verified? factory-url (:body asr))
                                 1
-                                0) (:badge_id user-badge)])))))
+                                0) (:badge_id user-badge)])
+        (if (and check-metabadge (not (clojure.string/blank? factory-url))) (check-metabadge ctx factory-url user-badge))))))
 
 (defn- check-badges [ctx db-conn factory-url]
   (log/info "check-badges: started working")
