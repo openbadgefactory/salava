a03a7327f48f23057fe2f1210db625dc5ad28fb1
isaakay111 <ogunlolui@gmail.com>
2019-10-30 10:42:33 +0200

    Small fix

--- src/cljs/salava/badge/ui/endorsement.cljs
+++ src/cljs/salava/badge/ui/endorsement.cljs
@@ -675,7 +675,7 @@
   [:div
    [m/modal-window]
    [:div#badge-stats
-    (if (or (seq @(cursor state [:received]) ) (seq @(cursor state [:given])) (pos? pending-requests-count))
+    (if (or (seq @(cursor state [:received]) ) (seq @(cursor state [:given])) (pos? pending-requests-count) (pos? sent-requests-count))
       [:div
        [:div.form-horizontal {:id "grid-filter"}
         [g/grid-search-field (t :core/Search ":")  "endorsementsearch" (t :badge/Filterbybadgenameoruser) :search state]
