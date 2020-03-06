af3626db9eefc7eab6f30678513afb7910eb763f
isaakay111 <ogunlolui@gmail.com>
2020-03-06 12:23:16 +0200

    Minor ui fixes

--- src/cljs/salava/badgeIssuer/ui/routes.cljs
+++ src/cljs/salava/badgeIssuer/ui/routes.cljs
@@ -17,18 +17,21 @@
    (str (base-path context) "/selfie") [[["/criteria/" :id] criteria/handler]]})
 
 (def about
-  {:selfie [:div#about-page
-             [:div
-              [:h5 [:b (t :badge/Whatisabadge)]]
-              [:p (t :badge/AnOpenBadgeIs)]]
-             [:p.page-tip [:em (t :badgeIssuer/Issuebadgespageinfo)]]]
-   :create [:div
-            [:p (t :badgeIssuer/Createnewbadgeinfo)]
-            [:p (t :badgeIssuer/Createnewbadgeinfo2)]]})
+  {:selfie {:heading (t :badgeIssuer/IssueBadges)
+            :content [:div
+                       [:div
+                        [:h5 [:b (t :badge/Whatisabadge)]]
+                        [:p (t :badge/AnOpenBadgeIs)]]
+                       [:p.page-tip [:em (t :badgeIssuer/Issuebadgespageinfo)]]]}
+
+   :create {:heading (t :badgeIssuer/Createselfiebadge)
+            :content [:div
+                      [:p (t :badgeIssuer/Createnewbadgeinfo)]
+                      [:p (t :badgeIssuer/Createnewbadgeinfo2)]]}})
 
 (defn ^:export navi [context]
-  {(str (base-path context) "/badge/selfie") {:weight 100 :title (t :badgeIssuer/IssueBadges) :site-navi true :breadcrumb (t :badge/Badges " / " :badgeIssuer/IssueBadges) :about {:heading (t :badgeIssuer/IssueBadges) :content (:selfie about)}}
-   (str (base-path context) "/badge/selfie/create") {:weight 101 :title (t :badgeIssuer/Createselfiebadges) :site-navi false :breadcrumb (t :badge/Badges " / " :badgeIssuer/Createselfiebadge)}})
+  {(str (base-path context) "/badge/selfie") {:weight 100 :title (t :badgeIssuer/IssueBadges) :site-navi true :breadcrumb (t :badge/Badges " / " :badgeIssuer/IssueBadges) :about (:selfie about)}
+   (str (base-path context) "/badge/selfie/create") {:weight 101 :title (t :badgeIssuer/Createselfiebadges) :site-navi false :breadcrumb (t :badge/Badges " / " :badgeIssuer/Createselfiebadge) :about (:create about)}})
    ;(str (base-path context) "/badge/selfie/create/\\S+")  {:breadcrumb (t :badge/Badges " / " :badgeIssuer/Editselfiebadge)}})
 
 (defn ^:export quicklinks []
