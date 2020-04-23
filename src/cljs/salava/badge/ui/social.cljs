81d9757f13adf408082581e727568f238564f115
isaakay111 <ogunlolui@gmail.com>
2020-04-23 19:43:48 +0300

    increase request endorsement link size

--- src/cljs/salava/badge/ui/social.cljs
+++ src/cljs/salava/badge/ui/social.cljs
@@ -133,7 +133,7 @@
             [:div.row.panel-title {:id "endorsebadge" :style {:margin-bottom "auto" :margin-top "10px"}}
              [:div.col-md-12.request-link [:a {:href "#"
                                                :on-click #(mo/open-modal [:badge :requestendorsement] {:state state :reload-fn (do (toggle-panel panel-identity visible-area-atom) (fn [] (get-pending-requests dataatom state)))})}
-                                           [:span [:i {:class (str "fa fa-fw " hand-icon)}] (t :badge/Requestendorsement)]]]]]
+                                           [:span {:style {:font-size "14.5px" :font-weight "600"}} [:i {:class (str "fa fa-fw " hand-icon)}] (t :badge/Requestendorsement)]]]]]
            (when (= @(cursor state [:visible-area]) panel-identity)
              [:div.panel-body.endorsements
               #_[:div.col-md-12.request-link {:id "endorsebadge"} [:a {:href "#"
