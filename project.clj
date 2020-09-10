23626ef29882c70cd81b6e0a28d0eaa446a711af
isaakay111 <ogunlolui@gmail.com>
2020-09-10 18:22:30 +0300

    version 3.13.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject salava "3.13.1"
+(defproject salava "3.13.2"
             :description "Salava application server"
             :url "http://salava.org"
             :license {:name "Apache 2.0"
