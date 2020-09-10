a8ce3eb98789a3902bea8ba93d2fdbdd18990242
isaakay111 <ogunlolui@gmail.com>
2020-09-10 14:40:02 +0300

    version 3.13.1

--- src/cljs/salava/extra/spaces/ui/routes.cljs
+++ src/cljs/salava/extra/spaces/ui/routes.cljs
@@ -41,7 +41,7 @@
   {(str (base-path context) "/space/admin") {:weight 150 :title (t :extra-spaces/Memberadmin) :top-navi true :breadcrumb (t :extra-spaces/Space " / " :admin/Admin)}
    (str (base-path context) "/space/stats") {:weight 500 :title (t :extra-spaces/Statistics) :site-navi true :breadcrumb (t :extra-spaces/Space " / " :extra-spaces/Statistics)}
    (str (base-path context) "/space/manage") {:weight 300 :title (t :extra-spaces/Manage)  :site-navi true :breadcrumb (t :extra-spaces/Space " / " :extra-spaces/Manage)}
-   (str (base-path context) "/space/users") {:weight 400 :title (t :extra-spaces/Users)  :site-navi true :breadcrumb (t :extra-spaces/Space " / " :extra-spaces/Users)}
+   (str (base-path context) "/space/users") {:weight 400 :title (t :extra-spaces/Members)  :site-navi true :breadcrumb (t :extra-spaces/Space " / " :extra-spaces/Members)}
    (str (base-path context) "/space/edit") {:weight 250 :title (t :extra-spaces/Edit)  :site-navi true :breadcrumb (t :extra-spaces/Space " / " :extra-spaces/Edit)}
    (str (base-path context) "/space/report") {:weight 650 :title (t :admin/Report)  :site-navi true :breadcrumb (t :extra-spaces/Space " / " :admin/Report)}})
 
